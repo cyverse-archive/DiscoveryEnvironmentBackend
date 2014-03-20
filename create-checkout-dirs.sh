cce3c4792647ab624d0aedd8798c9907fb3ec4a7
Dennis Roberts <dennis@iplantcollaborative.org>
2014-03-20 10:40:28 -0700

    converted some tabs to spaces

--- create-checkout-dirs.sh
+++ create-checkout-dirs.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 for project in $(find . -name checkouts.sh -exec dirname '{}' \;); do
-	pushd $project
-	if [ -d ./checkouts ]; then
-		rm -r ./checkouts
-	fi
+  pushd $project
+  if [ -d ./checkouts ]; then
+    rm -r ./checkouts
+  fi
 
-	./checkouts.sh
-    
-	if [ -f ./install-checkouts.sh ]; then
-		./install-checkouts.sh
-	fi
-	popd
+  ./checkouts.sh
+
+  if [ -f ./install-checkouts.sh ]; then
+    ./install-checkouts.sh
+  fi
+  popd
 done
