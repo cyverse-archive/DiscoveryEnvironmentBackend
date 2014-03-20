d018069dcfe778077f00ee60f70e9a0e5b4e64b2
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-03-19 19:14:17 -0700

    Yeah, um, oops.

--- get-versions.sh
+++ get-versions.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-for toplevel in services libs; do
-  pushd $toplevel 2>&1 > /dev/null
-  for d in $(ls); do
-    pushd $d 2>&1 > /dev/null
-    if [ -f project.clj ]; then
-      head -n 1 project.clj
-    fi
-    popd 2>&1 > /dev/null
-  done
-  popd 2>&1 > /dev/null
-done
+find . -name project.clj -exec head -n 1 '{}' \;
