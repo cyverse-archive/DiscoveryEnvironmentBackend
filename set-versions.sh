b282acb01339003d86798b0bd9ffd31b8178f490
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-03-19 19:29:25 -0700

    Fewer pushd-popd's and got rid of a nested if.

--- set-versions.sh
+++ set-versions.sh
@@ -9,20 +9,16 @@ echo "NOTE: This script requires the lein-set-version plugin for Leiningen."
 echo "I don't know if you have it already, but if you need it go to: "
 echo "https://github.com/pallet/lein-set-version"
 
-for toplevel in services libs; do
-  pushd $toplevel 2>&1 > /dev/null
-  for d in $(ls); do
-    pushd $d 2>&1 > /dev/null
-    if [ -f project.clj ]; then
-      if [ "$d" == "Donkey" ]; then
-        echo ">>> $d: Setting version to $1-SNAPSHOT"
-        lein set-version $1-SNAPSHOT
-      else
-        echo ">>> $d: Setting version to $1"
-        lein set-version $1
-      fi
-    fi
-    popd 2>&1 > /dev/null
-  done
-  popd 2>&1 > /dev/null
+for project in $(find . -name project.clj -exec dirname '{}' \;); do
+	pushd $project 2>&1 > /dev/null
+	cljproj=$(basename $project)
+	if [ "$cljproj" == "Donkey" ]; then
+		echo ">>> $cljproj: Setting version to $1-SNAPSHOT"
+		lein set-version $1-SNAPSHOT
+	else
+		echo ">>> $cljproj: Setting version to $1"
+		lein set-version $1
+	fi
+	popd 2>&1 > /dev/null
 done
+
