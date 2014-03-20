8e8797712e0b506e934ec24a5f11d32418890090
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-03-20 08:18:32 -0700

    Added a sed command to set the versions of deps in project.clj.

--- set-versions.sh
+++ set-versions.sh
@@ -11,14 +11,21 @@ echo "https://github.com/pallet/lein-set-version"
 
 for project in $(find . -name project.clj -exec dirname '{}' \;); do
 	pushd $project 2>&1 > /dev/null
+
 	cljproj=$(basename $project)
-	if [ "$cljproj" == "Donkey" ]; then
+
+  if [ "$cljproj" == "Donkey" ]; then
 		echo ">>> $cljproj: Setting version to $1-SNAPSHOT"
 		lein set-version $1-SNAPSHOT
 	else
 		echo ">>> $cljproj: Setting version to $1"
 		lein set-version $1
 	fi
-	popd 2>&1 > /dev/null
+
+  echo ">>> $cljproj: Setting versions of all iPlant deps to $1"
+  sed -i '' "s/^\(.*\[org\.iplantc\)\(.*\)\(\".*\"\)\(.*\)$/\1\2\"$1\"\4/g" project.clj
+  echo " "
+
+  popd 2>&1 > /dev/null
 done
 
