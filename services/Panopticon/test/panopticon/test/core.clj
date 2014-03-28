b99f24a15f10fcab7e61552c973a022b8be4d603
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-03-28 15:00:30 -0700

    Added unit tests for panopticon's config functions.

--- services/Panopticon/test/panopticon/test/core.clj
+++ services/Panopticon/test/panopticon/test/core.clj
@@ -6,4 +6,22 @@
  (boolize "true") => true
  (boolize "false") => false)
 
+(reset! props
+        {"panopticon.osm.url" "http://iplantcollaborative.org"
+         "panopticon.osm.collection" "collection"
+         "panopticon.condor.condor-config" "/etc/condor/condor-config"
+         "panopticon.condor.condor-q" "condor-q"
+         "panopticon.condor.condor-history" "condor-history"
+         "panopticon.app.num-instances" "1"
+         "panopticon.app.partition-size" "10"})
+
+(fact
+ (osm-url) => "http://iplantcollaborative.org"
+ (osm-coll) => "collection"
+ (condor-config) => "/etc/condor/condor-config"
+ (condor-q) => "condor-q"
+ (condor-history) => "condor-history"
+ (num-instances) => 1
+ (part-size) => 10)
+
 
