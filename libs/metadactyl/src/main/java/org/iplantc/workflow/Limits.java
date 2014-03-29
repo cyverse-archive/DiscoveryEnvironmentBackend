c66493fd641cf546db252c28083d1877ccadc244
Dennis Roberts <dennis@iplantcollaborative.org>
2014-03-28 18:11:26 -0700

    CORE-4811: when copying an app, don't let the new app name exceed its length limit

---
+++ libs/metadactyl/src/main/java/org/iplantc/workflow/Limits.java
@@ -0,0 +1,14 @@
+package org.iplantc.workflow;
+
+/**
+ * A class that defines limits in the workflow metadata.
+ */
+public class Limits {
+
+    /**
+     * Prevent instantiation.
+     */
+    private Limits() {}
+
+    public static int MAX_ANALYSIS_NAME_LENGTH = 255;
+}
