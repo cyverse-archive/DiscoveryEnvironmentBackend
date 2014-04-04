96e9975d7debda71aa5ebbf9a0328f9ddcdf146a
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-04-04 10:36:12 -0700

    Added the ability to slurp the MANIFEST.MF file.

--- libs/iplant-clojure-commons/src/clojure_commons/props.clj
+++ libs/iplant-clojure-commons/src/clojure_commons/props.clj
@@ -1,5 +1,7 @@
 (ns clojure-commons.props
   (:use [clojure.java.io :only (file input-stream)])
+  (:require [clojure.string :as string]
+            [trptcolin.versioneer.core :as version])
   (:import [java.net URLDecoder]
            [org.apache.commons.configuration PropertiesConfiguration]))
 
@@ -21,6 +23,12 @@
   [filename]
   (find-properties-file filename))
 
+(defn resource-stream
+  [filepath]
+  (.. (Thread/currentThread)
+      (getContextClassLoader)
+      (getResourceAsStream (file filepath))))
+
 (defn- find-config-file
   "Finds a configuration file, which may in tghe classpath or in the 'resources'
    subdirectory of the current working directory."
@@ -37,3 +45,31 @@
    library."
   [file-name]
   (PropertiesConfiguration. (find-config-file file-name)))
+
+(defn jar-domain
+  []
+  (.. (class *ns*)
+      (getProtectionDomain)
+      (getCodeSource)
+      (getLocation)))
+
+(defn jar-file-stream
+  [path-in-jar]
+  (.openStream
+   (java.net.URL.
+    (str "jar:"
+         (jar-domain)
+         path-in-jar))))
+
+(defn slurp-manifest
+  []
+  (slurp (jar-file-stream "!/META-INF/MANIFEST.MF")))
+
+(defn manifest
+  []
+  (or (slurp-manifest) ""))
+
+(defn version-info
+  [group-id artifact-id]
+  (str "App-Version: " (version/get-version group-id artifact-id) "\n"
+       (manifest)))
