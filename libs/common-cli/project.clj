4eaf88d880e8948b6b6f14a002f387ee3a019a83
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-04-07 09:58:58 -0700

    Initial commit of common-cli.

---
+++ libs/common-cli/project.clj
@@ -0,0 +1,6 @@
+(defproject common-cli "3.0.2"
+  :description "Common CLI functions for the DE backend services and tools"
+  :url "http://github.com/iPlantCollaborativeOpenSource/DiscoveryEnvironmentBackend/"
+  :license {:name "BSD"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [tools.cli "0.3.1"]])
