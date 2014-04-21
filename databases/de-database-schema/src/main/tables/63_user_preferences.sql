717d3793506132e2d9b528a2a79a581c4a6fe145
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-04-21 10:07:43 -0700

    CORE-5217: Added a database schema for the user preferences.

---
+++ databases/de-database-schema/src/main/tables/63_user_preferences.sql
@@ -0,0 +1,23 @@
+SET search_path = public, pg_catalog;
+
+--
+-- A table containing user preferences.
+--
+CREATE TABLE user_preferences (
+  id UUID UNIQUE NOT NULL,
+  user_id BIGINT UNIQUE NOT NULL REFERENCES users(id),
+  preferences JSON NOT NULL
+);
+
+---
+--- Creates an index on the id column.
+---
+CREATE INDEX user_preferences_id
+ON user_preferences(id);
+
+
+---
+--- Creates an index on the user_id column.
+---
+CREATE INDEX user_preferences_user_id
+ON user_preferences(user_id);
