5515860f64d56aa9c5ffd1f19adb2dbf4e996588
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-04-21 10:09:48 -0700

    Added a database conversion for creating the new session, preference, tree-url, and saved searches tables.

---
+++ databases/de-database-schema/src/main/conversions/c187_2014042101.clj
@@ -0,0 +1,76 @@
+(ns facepalm.c187-2014042101
+  (:use [korma.core])
+  (:import [java.util UUID]))
+
+(def ^:private version
+  "The destination database version."
+  "1.8.6:20140421.01")
+
+(defn- add-user-preferences-table
+  []
+  (println "\t* adding the user_preferences table")
+  (exec-raw
+   "CREATE TABLE user_preferences (
+        id UUID UNIQUE NOT NULL,
+        user_id BIGINT UNIQUE NOT NULL REFERENCES users(id),
+        preferences JSON NOT NULL
+    )")
+  (exec-raw
+   "CREATE INDEX user_preferences_id
+    ON user_preferences(id)")
+  (exec-raw
+   "CREATE INDEX user_preferences_user_id
+    ON user_preferences(user_id)"))
+
+(defn- add-sessions-table
+  []
+  (println "\t* adding the user_sessions table")
+  (exec-raw
+   "CREATE TABLE user_sessions (
+        id UUID UNIQUE NOT NULL,
+        user_id BIGINT UNIQUE NOT NULL REFERENCES users(id);
+        session JSON NOT NULL,
+    )")
+  (exec-raw
+   "CREATE INDEX user_sessions_id
+    ON user_sessions(id)")
+  (exec-raw
+   "CREATE INDEX user_session_user_id
+    ON user_sessions(user_id)"))
+
+(defn- add-saved-searches-table
+  []
+  (println "\t* adding the user_saved_searches table")
+  (exec-raw
+   "CREATE TABLE user_saved_searches (
+        id UUID UNIQUE NOT NULL,
+        user_id BIGINT UNIQUE NOT NULL REFERENCES users(id),
+        saved_searches JSON NOT NULL
+    )")
+  (exec-raw
+   "CREATE INDEX user_saved_searches_id
+    ON user_saved_searches(id)")
+  (exec-raw
+   "CREATE INDEX user_saved_searches_user_id
+    ON user_saved_searches(user_id)"))
+
+(defn- add-tree-urls-table
+  []
+  (println "\t* adding the tree_urls table")
+  (exec-raw
+   "CREATE TABLE tree_urls (
+        id UUID UNIQUE NOT NULL,
+        tree_urls JSON NOT NULL
+    )")
+  (exec-raw
+   "CREATE INDEX tree_urls_id
+    ON tree_urls(id)"))
+
+(defn convert
+  "Performs the conversion for database version 1.8.4:20140421.01."
+  []
+  (println "Performing conversion for " version)
+  (add-user-preferences-table)
+  (add-sessions-table)
+  (add-saved-searches-table)
+  (add-tree-urls-table))
