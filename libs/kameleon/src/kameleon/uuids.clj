9674f826fb9e2e7e8668d563584b4563afaa0407
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-04-23 09:37:14 -0700

    Added utility functions for uuids.

--- libs/kameleon/src/kameleon/uuids.clj
+++ libs/kameleon/src/kameleon/uuids.clj
@@ -3,3 +3,19 @@
 (defn uuid
   []
   (java.util.UUID/randomUUID))
+
+(defmulti uuidify
+  (fn [obj] (type obj)))
+
+(defmethod uuidify java.util.UUID
+  [uuid]
+  uuid)
+
+(defmethod uuidify java.lang.String
+  [uuid]
+  (java.util.UUID/fromString uuid))
+
+(defmethod uuidify :default
+  [uuid]
+  nil)
+
