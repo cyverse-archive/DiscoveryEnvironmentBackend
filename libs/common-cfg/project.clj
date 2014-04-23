e400ed5ebb7a93dd414cbd6a415688b1452e1368
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-04-23 14:48:27 -0700

    Initial commit of common-cfg.

---
+++ libs/common-cfg/project.clj
@@ -0,0 +1,8 @@
+(defproject org.iplantc/common-cfg "3.1.0"
+  :description "DE services code for managing configurations."
+  :url "http://github.com/iPlantCollaborativeOpenSource/DiscoveryEnvironmentBackend/"
+  :license {:name "BSD"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.taoensso/timbre "3.1.6"]
+                 [filevents "0.1.0"]
+                 [bouncer "0.3.1-beta1"]])
