aa7aa30555968ee4ff3eb35f94cfc4e0e4da51b9
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-04-30 12:06:17 -0700

    Modified the session deletion to return consistent values.

--- services/user-sessions/src/user_sessions/serve.clj
+++ services/user-sessions/src/user_sessions/serve.clj
@@ -52,9 +52,11 @@
 
 (defn delete-req
   [username req]
-  (validate
-   [username req false]
-   (response (sanitize (delete-user-session username)))))
+  (if-not (user? username)
+    ""
+    (do
+      (delete-user-session username)
+      "")))
 
 (defn put-req
   [username req]
