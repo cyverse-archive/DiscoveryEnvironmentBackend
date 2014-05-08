f60aaf8dc65b070c722c956a7b28047d2750e7be
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-05-08 09:18:34 -0700

    CORE-5479: Modified the metadata endpoints to restrict access to the AVUs that start with 'ipc'.

--- services/Donkey/src/donkey/services/filesystem/metadata.clj
+++ services/Donkey/src/donkey/services/filesystem/metadata.clj
@@ -22,13 +22,30 @@
     (assoc avu :unit "")
     avu))
 
+(def ipc-regex #"(?i)^ipc")
+
+(defn ipc-avu?
+  "Returns a truthy value if the AVU map passed in is reserved for the DE's use."
+  [avu]
+  (or (re-find ipc-regex (:attr avu))
+      (re-find ipc-regex (:unit avu))))
+
+(defn authorized-avus
+  "Validation to make sure the AVUs aren't system AVUs. Throws a slingshot error
+   map if the validation fails."
+  [avus]
+  (when (some ipc-avu? avus)
+    (throw+ {:error_code ERR_NOT_AUTHORIZED
+             :avus avus})))
+
 (defn- list-path-metadata
   "Returns the metadata for a path. Passes all AVUs to (fix-unit).
    AVUs with a unit matching IPCSYSTEM are filtered out."
   [cm path]
-  (filterv
-   #(not= (:unit %) IPCSYSTEM)
-   (map fix-unit (get-metadata cm (ft/rm-last-slash path)))))
+  (let [ipc-regex #"(?i)^ipc"]
+    (filterv
+     #(not (ipc-avu? %))
+     (map fix-unit (get-metadata cm (ft/rm-last-slash path))))))
 
 (defn- reserved-unit
   "Turns a blank unit into a reserved unit."
@@ -84,6 +101,7 @@
       (throw+ {:error_code ERR_INVALID_JSON}))
     (validators/path-exists cm path)
     (validators/path-writeable cm user path)
+    (authorized-avus [avu-map])
     {:path (common-metadata-set cm path avu-map)
      :user user}))
 
@@ -127,6 +145,8 @@
     (validators/user-exists cm user)
     (validators/path-exists cm path)
     (validators/path-writeable cm user path)
+    (authorized-avus (:delete adds-dels))
+    (authorized-avus (:add adds-dels))
     (let [new-path (ft/rm-last-slash path)]
       (doseq [del (:delete adds-dels)]
         (let [attr  (:attr del)
@@ -148,6 +168,7 @@
     (validators/user-exists cm user)
     (validators/path-exists cm path)
     (validators/path-writeable cm user path)
+    (authorized-avus [{:attr attr :value value :unit ""}])
     (delete-metadata cm path attr value)
     {:path path :user user}))
 
