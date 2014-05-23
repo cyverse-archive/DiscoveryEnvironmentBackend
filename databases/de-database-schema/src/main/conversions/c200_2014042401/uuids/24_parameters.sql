485b50ac803a277475ed9b8320cc56f521ea27cb
Paul Sarando <psarando@iplantcollaborative.org>
2014-05-22 17:06:21 -0700

    CORE-5386 Fixed parameters UUID conversions.

--- databases/de-database-schema/src/main/conversions/c200_2014042401/uuids/24_parameters.sql
+++ databases/de-database-schema/src/main/conversions/c200_2014042401/uuids/24_parameters.sql
@@ -2,9 +2,19 @@ SET search_path = public, pg_catalog;
 
 --
 -- Updates parameters uuid foreign keys.
+-- Adds temporary indexes to help speed up the conversion.
 --
-UPDATE validation_rules SET parameter_id =
+CREATE INDEX parameters_validator_idx ON parameters(validator);
+CREATE INDEX validator_rule_validator_id_idx ON validator_rule(validator_id);
+CREATE INDEX validator_rule_rule_id_idx ON validator_rule(rule_id);
+
+UPDATE validation_rules r SET parameter_id =
     (SELECT p.id FROM parameters p
      LEFT JOIN validator_rule vr ON vr.validator_id = p.validator
-     WHERE hid = rule_id);
+     WHERE r.hid = vr.rule_id);
+
+-- Drop temporary indexes.
+DROP INDEX parameters_validator_idx;
+DROP INDEX validator_rule_validator_id_idx;
+DROP INDEX validator_rule_rule_id_idx;
 
