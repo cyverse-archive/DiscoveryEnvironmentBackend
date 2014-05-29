685e30377871ca8c179a3f66b241659be9de1e97
Dennis Roberts <dennis@iplantcollaborative.org>
2014-05-29 16:04:17 -0700

    CORE-5546: attempt to refresh the access token if Agave gives us a 401 status

--- libs/mescal/src/mescal/agave_v2.clj
+++ libs/mescal/src/mescal/agave_v2.clj
@@ -1,30 +1,49 @@
 (ns mescal.agave-v2
+  (:use [slingshot.slingshot :only [throw+ try+]])
   (:require [authy.core :as authy]
             [cemerick.url :as curl]
             [clj-http.client :as http]
             [mescal.util :as util]
             [clojure.tools.logging :as log]))
 
+(defn- refresh-access-token
+  [token-info-fn timeout]
+  (let [new-token-info (authy/refresh-access-token @(token-info-fn) :timeout timeout)]
+    (dosync (ref-set (token-info-fn) new-token-info))))
+
+(defn- wrap-refresh
+  [token-info-fn timeout request-fn]
+  (try+
+   (request-fn)
+   (catch [:status 401] _
+     (refresh-access-token token-info-fn timeout)
+     (request-fn))))
+
+(defmacro ^:private with-refresh
+  [[token-info-fn timeout] & body]
+  `(wrap-refresh ~token-info-fn ~timeout #(do ~@body)))
+
 (defn check-access-token
   [token-info-fn timeout]
   (when (authy/token-expiring? @(token-info-fn))
-    (let [new-token-info (authy/refresh-access-token @(token-info-fn) :timeout timeout)]
-      (dosync (ref-set (token-info-fn) new-token-info)))))
+    (refresh-access-token token-info-fn timeout)))
 
 (defn list-systems
   [base-url token-info-fn timeout]
-  ((comp :result :body)
-   (http/get (str (curl/url base-url "/systems/v2/"))
-             {:oauth-token    (:access-token @(token-info-fn))
-              :as             :json
-              :conn-timeout   timeout
-              :socket-timeout timeout})))
+  (with-refresh [token-info-fn timeout]
+    ((comp :result :body)
+     (http/get (str (curl/url base-url "/systems/v2/"))
+               {:oauth-token    (:access-token @(token-info-fn))
+                :as             :json
+                :conn-timeout   timeout
+                :socket-timeout timeout}))))
 
 (defn list-apps
   [base-url token-info-fn timeout]
-  ((comp :result :body)
-   (http/get (str (curl/url base-url "/apps/v2/"))
-             {:oauth-token    (:access-token @(token-info-fn))
-              :as             :json
-              :conn-timeout   timeout
-              :socket-timeout timeout})))
+  (with-refresh [token-info-fn timeout]
+    ((comp :result :body)
+     (http/get (str (curl/url base-url "/apps/v2/"))
+               {:oauth-token    (:access-token @(token-info-fn))
+                :as             :json
+                :conn-timeout   timeout
+                :socket-timeout timeout}))))
