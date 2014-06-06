582cc7837c1cb3b2ec083f765d7399d8216611d0
Tony Edgin <tedgin@iplantcollaborative.org>
2014-06-06 14:11:46 -0700

    CORE-5598 converted targets.type to an enumerated type

--- databases/metadata/src/main/tables/targets.sql
+++ databases/metadata/src/main/tables/targets.sql
@@ -1,10 +1,17 @@
 SET search_path = public, pg_catalog;
 
+
+--
+-- target type enumeration
+-- Currently, only analyses, apps, data and users may be targets of metadata
+--
+CREATE TYPE target_type AS ENUM ('analysis', 'app', 'data', 'user');
+
 --
 -- targets table
 --
 CREATE TABLE targets (
   id UUID NOT NULL,
-  type varchar NOT NULL
+  type target_type NOT NULL
 );
 
