e6b17037efc580a535826188da2c7b92c7251a7a
Tony Edgin <tedgin@iplantcollaborative.org>
2014-06-09 15:58:58 -0700

    CORE-5598 Added (value, owner_id) uniqueness constraint

--- databases/metadata/src/main/constraints/01_tags.sql
+++ databases/metadata/src/main/constraints/01_tags.sql
@@ -7,3 +7,9 @@ ALTER TABLE tags
     ADD CONSTRAINT tags_pkey
     PRIMARY KEY (id);
 
+--
+-- values have to be unique per user
+--
+ALTER TABLE tags
+    ADD CONSTRAINT tags_unique_value_user
+    UNIQUE (value, owner_id);
