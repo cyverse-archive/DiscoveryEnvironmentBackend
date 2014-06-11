b5e85e8c62832ddb620d2a1bd0ef9971f8ad11d0
Dennis Roberts <dennis@iplantcollaborative.org>
2014-06-11 16:40:26 -0700

    CORE-5614: changed the column type for storing access tokens in the database to "byte array"

--- databases/de-database-schema/src/main/tables/67_access_tokens.sql
+++ databases/de-database-schema/src/main/tables/67_access_tokens.sql
@@ -6,7 +6,7 @@ SET search_path = public, pg_catalog;
 CREATE TABLE access_tokens (
     webapp VARCHAR(64) NOT NULL,
     user_id BIGINT NOT NULL,
-    token VARCHAR(128) NOT NULL,
+    token BYTEA NOT NULL,
     expires_at TIMESTAMP,
-    refresh_token VARCHAR(128)
+    refresh_token BYTEA
 );
