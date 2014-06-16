41842241f4198011e42b7137a8821df7cc4b96c4
Paul Sarando <psarando@iplantcollaborative.org>
2014-06-16 16:14:45 -0700

    CORE-5602 Moved metadata template avu functions.
    Moved these service functions into the donkey.services.filesystem.metadata-templates namespace.

--- services/Donkey/src/donkey/services/filesystem/metadata_templates.clj
+++ services/Donkey/src/donkey/services/filesystem/metadata_templates.clj
@@ -1,8 +1,17 @@
 (ns donkey.services.filesystem.metadata-templates
-  (:use [donkey.services.filesystem.common-paths]
+  (:use [clj-jargon.init :only [with-jargon]]
+        [donkey.services.filesystem.common-paths]
+        [donkey.util.config]
         [korma.core]
-        [korma.db :only [with-db]])
-  (:require [dire.core :refer [with-pre-hook! with-post-hook!]]
+        [korma.db :only [transaction with-db]]
+        [slingshot.slingshot :only [throw+]])
+  (:require [clojure-commons.error-codes :as error-codes]
+            [clojure-commons.validators :as common-validators]
+            [clojure.tools.logging :as log]
+            [dire.core :refer [with-pre-hook! with-post-hook!]]
+            [donkey.persistence.metadata :as persistence]
+            [donkey.services.filesystem.uuids :as uuids]
+            [donkey.services.filesystem.validators :as validators]
             [donkey.util.db :as db]
             [donkey.util.service :as service])
   (:import [java.util UUID]))
@@ -95,3 +104,157 @@
     (log-call "do-metadata-attribute-view" id)))
 
 (with-post-hook! #'do-metadata-attribute-view (log-func "do-metadata-attribute-view"))
+
+(defn- get-metadata-template-avus
+  "Gets a map containing AVUs for the given Metadata Template and the template's ID."
+  [user-id data-id template-id]
+  (let [avus (persistence/get-avus-for-metadata-template user-id data-id template-id)]
+    {:template_id template-id
+     :avus (map #(dissoc % :target_type) avus)}))
+
+(defn- metadata-template-list
+  "Lists all Metadata Template AVUs for the given user's data item."
+  [user-id data-id]
+  (let [template-ids (persistence/get-metadata-template-ids user-id data-id)]
+    {:user user-id
+     :data_id data-id
+     :templates (map (comp (partial get-metadata-template-avus user-id data-id) :template_id)
+                     template-ids)}))
+
+(defn- metadata-template-avu-list
+  "Lists AVUs for the given Metadata Template on the given user's data item."
+  [user-id data-id template-id]
+  (assoc (get-metadata-template-avus user-id data-id template-id)
+    :user user-id
+    :data_id data-id))
+
+(defn- format-avu
+  "Formats the given AVU for adding or updating.
+   If the AVU already exists, the result will contain its ID."
+  [user-id data-id avu]
+  (let [avu (-> (select-keys avu [:value :unit])
+                (assoc
+                  :id (when (:id avu) (UUID/fromString (:id avu)))
+                  :target_id data-id
+                  :owner_id user-id
+                  :attribute (:attr avu)))
+        existing-avu (persistence/find-existing-metadata-template-avu avu)]
+    (if existing-avu
+      (assoc avu :id (:id existing-avu))
+      avu)))
+
+(defn- set-metadata-template-avus
+  "Adds or Updates AVUs associated with a Metadata Template for the given user's data item."
+  [user-id data-id template-id {avus :avus}]
+  (let [avus (map (partial format-avu user-id data-id) avus)
+        existing-avus (filter :id avus)
+        new-avus (map #(assoc % :id (UUID/randomUUID)) (remove :id avus))
+        avus (concat existing-avus new-avus)]
+    (transaction
+     (when (seq existing-avus)
+       (log/debug "Updating existing avus:" (map :id existing-avus))
+       (dorun (map persistence/update-avu existing-avus)))
+     (when (seq new-avus)
+       (log/debug "Inserting new avus:" (map :id new-avus))
+       (persistence/add-metadata-template-avus new-avus))
+     (dorun (persistence/add-template-instances template-id (map :id avus))))
+    {:user user-id
+     :data_id data-id
+     :template_id template-id
+     :avus (map #(select-keys % [:id :attribute :value :unit]) avus)}))
+
+(defn- remove-metadata-template-avu
+  "Removes the given Metadata Template AVU association for the given user's data item."
+  [user-id data-id template-id avu-id]
+  (let [avu {:id avu-id
+             :target_id data-id
+             :owner_id user-id}
+        existing-avu (persistence/find-existing-metadata-template-avu avu)]
+    (if existing-avu
+      (transaction
+       (persistence/remove-avu-template-instances template-id [avu-id])
+       (persistence/remove-avu avu-id))
+      (throw+ {:error_code error-codes/ERR_DOES_NOT_EXIST
+               :avu avu})))
+  (service/success-response))
+
+(defn- remove-metadata-template-avus
+  "Removes AVUs associated with the given Metadata Template for the given user's data item."
+  [user-id data-id template-id]
+  (let [avu-ids (map :id (persistence/get-avus-for-metadata-template user-id data-id template-id))]
+    (transaction
+     (persistence/remove-avu-template-instances template-id avu-ids)
+     (persistence/remove-avus avu-ids))
+    (service/success-response)))
+
+(defn do-metadata-template-avu-list
+  "Lists AVUs associated with a Metadata Template for the given user's data item."
+  ([{user :user} data-id]
+   (metadata-template-list user (UUID/fromString data-id)))
+
+  ([{user :user} data-id template-id]
+   (metadata-template-avu-list user
+                               (UUID/fromString data-id)
+                               (UUID/fromString template-id))))
+
+(with-pre-hook! #'do-metadata-template-avu-list
+  (fn [params data-id & [template-id]]
+    (log-call "do-metadata-template-avu-list" params data-id template-id)
+    (with-jargon (jargon-cfg) [cm]
+      (common-validators/validate-map params {:user string?})
+      (let [user (:user params)
+            path (:path (uuids/path-for-uuid cm user data-id))]
+        (validators/user-exists cm user)
+        (validators/path-exists cm path)
+        (validators/path-readable cm user path)))))
+
+(with-post-hook! #'do-metadata-template-avu-list (log-func "do-metadata-template-avu-list"))
+
+(defn do-set-metadata-template-avus
+  "Adds or Updates AVUs associated with a Metadata Template for the given user's data item."
+  [{username :user} data-id template-id body]
+  (set-metadata-template-avus username
+                              (UUID/fromString data-id)
+                              (UUID/fromString template-id)
+                              body))
+
+(with-pre-hook! #'do-set-metadata-template-avus
+  (fn [params data-id template-id body]
+    (log-call "do-set-metadata-template-avus" params data-id template-id body)
+    (with-jargon (jargon-cfg) [cm]
+      (common-validators/validate-map body {:avus sequential?})
+      (common-validators/validate-map params {:user string?})
+      (let [user (:user params)
+            path (:path (uuids/path-for-uuid cm user data-id))]
+        (validators/user-exists cm user)
+        (validators/path-exists cm path)
+        (validators/path-readable cm user path)))))
+
+(with-post-hook! #'do-set-metadata-template-avus (log-func "do-set-metadata-template-avus"))
+
+(defn do-remove-metadata-template-avus
+  "Removes AVUs associated with a Metadata Template for the given user's data item."
+  ([{user :user} data-id template-id]
+   (remove-metadata-template-avus user
+                                  (UUID/fromString data-id)
+                                  (UUID/fromString template-id)))
+
+  ([{user :user} data-id template-id avu-id]
+   (remove-metadata-template-avu user
+                                 (UUID/fromString data-id)
+                                 (UUID/fromString template-id)
+                                 (UUID/fromString avu-id))))
+
+(with-pre-hook! #'do-remove-metadata-template-avus
+  (fn [params data-id template-id & [avu-id]]
+    (log-call "do-remove-metadata-template-avus" params data-id template-id avu-id)
+    (with-jargon (jargon-cfg) [cm]
+      (common-validators/validate-map params {:user string?})
+      (let [user (:user params)
+            path (:path (uuids/path-for-uuid cm user data-id))]
+        (validators/user-exists cm user)
+        (validators/path-exists cm path)
+        (validators/path-readable cm user path)))))
+
+(with-post-hook! #'do-remove-metadata-template-avus (log-func "do-remove-metadata-template-avus"))
+
