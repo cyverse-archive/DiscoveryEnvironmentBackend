90e5fde1917622053eb47f5e3079f073ecfc9565
Dennis Roberts <dennis@iplantcollaborative.org>
2014-06-24 10:27:56 -0700

    CORE-5760: modified the listings for Agave apps to allow them to be included in pipelines

--- libs/mescal/src/mescal/agave_de_v2/app_listings.clj
+++ libs/mescal/src/mescal/agave_de_v2/app_listings.clj
@@ -39,7 +39,7 @@
             :integrator_name      c/unknown-value
             :is_favorite          false
             :is_public            (:isPublic listing)
-            :pipeline_eligibility {:is_valid false :reason "HPC App"}
+            :pipeline_eligibility {:is_valid true}
             :rating               {:average 0.0}
             :step-count           1
             :wiki_url             ""))))
