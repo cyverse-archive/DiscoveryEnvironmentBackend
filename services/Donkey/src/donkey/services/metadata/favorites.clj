226cbe2051d88f7611c493462f75106fc439e93f
Tony Edgin <tedgin@iplantcollaborative.org>
2014-06-26 12:13:31 -0700

    documented favorites

--- services/Donkey/src/donkey/services/metadata/favorites.clj
+++ services/Donkey/src/donkey/services/metadata/favorites.clj
@@ -12,25 +12,11 @@
   (:import [java.util UUID]))
 
 
-(defn add-favorite
-  [entry-id]
-  (let [user     (:shortUsername user/current-user)
-        entry-id (UUID/fromString entry-id)]
-    (fs/with-jargon (cfg/jargon-cfg) [cm]
-      (uuids/validate-uuid-accessible cm user entry-id))
-    (when-not (db/is-favorite user entry-id)
-      (db/insert-favorite user entry-id "data"))
-    (svc/success-response)))
-
-(defn remove-favorite
-  [entry-id]
-  (let [user     (:shortUsername user/current-user)
-        entry-id (UUID/fromString entry-id)]
-  (if (db/is-favorite user entry-id)
-    (do
-      (db/delete-favorite user entry-id)
-      (svc/success-response))
-    (svc/donkey-response {} 404))))
+(defn- format-favorites
+  [favs]
+  (let [favs (map #(assoc % :isFavorite true) favs)]
+    {:folders (filter #(= (:type %) :dir) favs)
+     :files   (filter #(= (:type %) :file) favs)}))
 
 (defn- user-col->api-col
   [col]
@@ -49,14 +35,48 @@
     "DESC" :desc
     :asc))
 
-(defn- format-favorites
-  [favs]
-  (let [favs (map #(assoc % :isFavorite true) favs)]
-    {:folders (filter #(= (:type %) :dir) favs)
-     :files   (filter #(= (:type %) :file) favs)}))
+(defn add-favorite
+  "This function marks a given filesystem entry as a favorite of the authenticated user.
+
+   Parameters:
+     entry-id - This is the `entry-id` from the request.  It should be the UUID of the entry being
+                marked."
+  [entry-id]
+  (let [user     (:shortUsername user/current-user)
+        entry-id (UUID/fromString entry-id)]
+    (fs/with-jargon (cfg/jargon-cfg) [cm]
+      (uuids/validate-uuid-accessible cm user entry-id))
+    (when-not (db/is-favorite user entry-id)
+      (db/insert-favorite user entry-id "data"))
+    (svc/success-response)))
+
+(defn remove-favorite
+  "This function unmarks a given filesystem entry as a favortie of the authenticated user.
+
+   Parameters:
+     entry-id - This is the `entry-id` from the request.  It should be the UUID of the entry being
+                unmarked."
+  [entry-id]
+  (let [user     (:shortUsername user/current-user)
+        entry-id (UUID/fromString entry-id)]
+  (if (db/is-favorite user entry-id)
+    (do
+      (db/delete-favorite user entry-id)
+      (svc/success-response))
+    (svc/donkey-response {} 404))))
 
 (defn list-favorite-data-with-stat
-  "Returns a listing of a user's favorite data, including stat information about it."
+  "Returns a listing of a user's favorite data, including stat information about it. This endpoint
+   is intended to help with paginating.
+
+   Parameters:
+     sort-col - This is the value of the `sort-col` query parameter. It should be a case-insensitive
+                string containing one of the following: DATECREATED|ID|LASTMODIFIED|NAME|SIZE
+     sort-order - This is the value of the `sort-order` query parameter. It should be a
+                case-insensitive string containing one of the following: ASC|DESC
+     limit - This is the value of the `limit` query parameter. It should contain a positive number.
+     offset - This is the value of the `offset` query parameter. It should contain a non-negative
+              number."
   [sort-col sort-order limit offset]
   (let [user         (:shortUsername user/current-user)
         col          (user-col->api-col sort-col)
@@ -72,6 +92,12 @@
       svc/success-response)))
 
 (defn filter-favorites
+  "Given a list of UUIDs for filesystem entries, it filters the list, returning only the UUIDS that
+   are marked as favorite by the authenticated user.
+
+   Parameters:
+     body - This is the request body. It should contain a JSON document containing a field
+            `filesystem` containing an array of UUIDs."
   [body]
   (let [user    (:shortUsername user/current-user)
         ids-txt (-> body slurp (json/parse-string true) :filesystem)
