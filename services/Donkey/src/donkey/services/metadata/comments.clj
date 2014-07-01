2e7c980d7856427c56653b7a2b7fbd961d38e978
Tony Edgin <tedgin@iplantcollaborative.org>
2014-07-01 11:31:17 -0700

    list comments can now respond with 404
    It will respond with a 404 if the entry identified in the URL doesn't exist, isn't a UUID or isn't accessible by the authenticated user.

--- services/Donkey/src/donkey/services/metadata/comments.clj
+++ services/Donkey/src/donkey/services/metadata/comments.clj
@@ -1,9 +1,9 @@
 (ns donkey.services.metadata.comments
-  (:use [slingshot.slingshot :only [try+ throw+]])
+  (:use [slingshot.slingshot :only [try+ throw+]]
+        [clojure-commons.error-codes])
   (:require [cheshire.core :as json]
             [clj-jargon.init :as fs-init]
             [clj-jargon.permissions :as fs-perm]
-            [clojure-commons.error-codes :as err]
             [donkey.auth.user-attributes :as user]
             [donkey.persistence.metadata :as db]
             [donkey.util.config :as config]
@@ -24,13 +24,18 @@
   (try+
     (slurp stream)
     (catch OutOfMemoryError _
-      (throw+ {:error_code err/ERR_REQUEST_BODY_TOO_LARGE}))))
+      (throw+ {:error_code ERR_REQUEST_BODY_TOO_LARGE}))))
 
 
-(defn- validate-entry-accessible
-  [fs-cfg user entry-id]
-  (fs-init/with-jargon fs-cfg [fs]
-    (uuid/validate-uuid-accessible fs user entry-id)))
+(defn- extract-entry-id
+  [fs-cfg user entry-id-txt]
+  (try+
+    (let [entry-id (UUID/fromString entry-id-txt)]
+      (fs-init/with-jargon fs-cfg [fs]
+        (uuid/validate-uuid-accessible fs user entry-id))
+      entry-id)
+    (catch [:error_code ERR_DOES_NOT_EXIST] _ (throw+ {:error_code ERR_NOT_FOUND}))
+    (catch IllegalArgumentException _ (throw+ {:error_code ERR_NOT_FOUND}))))
 
 
 (defn add-comment
@@ -43,15 +48,12 @@
   [entry-id body]
   (try+
     (let [user     (:shortUsername user/current-user)
-          entry-id (UUID/fromString entry-id)
+          entry-id (extract-entry-id (config/jargon-cfg) user entry-id)
           comment  (-> body read-body (json/parse-string true) :comment)]
-      (validate-entry-accessible (config/jargon-cfg) user entry-id)
-      (when-not comment (throw+ {:error_code err/ERR_INVALID_JSON}))
+      (when-not comment (throw+ {:error_code ERR_INVALID_JSON}))
       (let [comment (db/insert-comment  user entry-id "data" comment)]
         (svc/create-response {:comment (prepare-post-time comment)})))
-    (catch [:error_code err/ERR_DOES_NOT_EXIST] _ (throw+ {:error_code  err/ERR_NOT_FOUND}))
-    (catch IllegalArgumentException _ (throw+ {:error_code  err/ERR_NOT_FOUND}))
-    (catch JsonParseException _ (throw+ {:error_code err/ERR_INVALID_JSON}))))
+    (catch JsonParseException _ (throw+ {:error_code ERR_INVALID_JSON}))))
 
 
 (defn list-comments
@@ -61,10 +63,8 @@
    Parameters:
      entry-id - the `entry-id` from the request. This should be the UUID corresponding to the entry
                 being inspected"
-  (let [user     (:shortUsername user/current-user)
-        entry-id (UUID/fromString entry-id)]
-  (validate-entry-accessible (config/jargon-cfg) user entry-id)
-  (svc/success-response {:comments (map prepare-post-time (db/select-all-comments entry-id))})))
+  (let [entry-id (extract-entry-id (config/jargon-cfg) (:shortUsername user/current-user) entry-id)]
+    (svc/success-response {:comments (map prepare-post-time (db/select-all-comments entry-id))})))
 
 
 (defn update-retract-status
