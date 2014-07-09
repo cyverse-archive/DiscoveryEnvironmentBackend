f11a12bd16e284b1cc38b66fd3a51a4533a020d4
Dennis Roberts <dennis@iplantcollaborative.org>
2014-07-09 15:10:37 -0700

    CORE-5772: reformatted JobConstructor

--- libs/metadactyl/src/main/java/org/iplantc/workflow/experiment/dto/JobConstructor.java
+++ libs/metadactyl/src/main/java/org/iplantc/workflow/experiment/dto/JobConstructor.java
@@ -7,70 +7,65 @@ import org.iplantc.workflow.util.SfJsonUtils;
 /**
  * Helper class used to build a job.  Used to fill out parts of the Job object
  * with values from other objects.
- *
+ * <p/>
  * When this class is constructor it automatically creates a new Job
  * internally.  Use <code>set..()</code> methods to construct the Job.  These
  * will take the passed in objects and transfer values form them to the
  * actual underlying Job object.
  *
- * @see JobDto
- *
  * @author Kris Healy &lt;healyk@iplantcollaborative.org&gt;
+ * @see JobDto
  */
 public class JobConstructor {
-  private JobDto job;
+    private JobDto job;
 
-  /**
-   * Constructs a new JobConstructor.  This will internally create a new
-   * Job.
-   *
-   * @param requestType
-   *  Job's request type
-   * @param targetExecutor
-   *  Job's target executor
-   */
-  public JobConstructor(String requestType, String targetExecutor) {
-    job = new JobDto(requestType, targetExecutor);
-  }
+    /**
+     * Constructs a new JobConstructor.  This will internally create a new
+     * Job.
+     *
+     * @param requestType    Job's request type
+     * @param targetExecutor Job's target executor
+     */
+    public JobConstructor(String requestType, String targetExecutor) {
+        job = new JobDto(requestType, targetExecutor);
+    }
 
-  /**
-   * Sets the experiment json info on the internal Job.
-   *
-   * @param experimentJson the JSON to generate the job DTO from.
-   */
-  public void setExperimentJson(JSONObject experimentJson) {
-    job.setName(experimentJson.getString("name"));
-    job.setDisplayName(experimentJson.optString("display_name"));
-    job.setDescription(experimentJson.optString("description", ""));
-    job.setNotify(experimentJson.getBoolean("notify"));
-    job.setWorkspaceId(experimentJson.getString("workspace_id"));
-    job.setOutputDir(SfJsonUtils.optString(experimentJson, "", "outputDirectory", "output_dir"));
-    job.setCreateOutputSubdir(experimentJson.optBoolean("create_output_subdir", true));
-  }
+    /**
+     * Sets the experiment json info on the internal Job.
+     *
+     * @param experimentJson the JSON to generate the job DTO from.
+     */
+    public void setExperimentJson(JSONObject experimentJson) {
+        job.setName(experimentJson.getString("name"));
+        job.setDisplayName(experimentJson.optString("display_name"));
+        job.setDescription(experimentJson.optString("description", ""));
+        job.setNotify(experimentJson.getBoolean("notify"));
+        job.setWorkspaceId(experimentJson.getString("workspace_id"));
+        job.setOutputDir(SfJsonUtils.optString(experimentJson, "", "outputDirectory", "output_dir"));
+        job.setCreateOutputSubdir(experimentJson.optBoolean("create_output_subdir", true));
+    }
 
-  /**
-   * Sets the Analysis variables on the Job object.
-   *
-   * @param transformationActivity
-   *  Activity to pull the analysis data from.
-   */
-  public void setAnalysis(TransformationActivity transformationActivity) {
-    job.setAnalysisDescription(transformationActivity.getDescription());
-    job.setAnalysisId(transformationActivity.getId());
-    job.setAnalysisName(transformationActivity.getName());
-  }
+    /**
+     * Sets the Analysis variables on the Job object.
+     *
+     * @param transformationActivity Activity to pull the analysis data from.
+     */
+    public void setAnalysis(TransformationActivity transformationActivity) {
+        job.setAnalysisDescription(transformationActivity.getDescription());
+        job.setAnalysisId(transformationActivity.getId());
+        job.setAnalysisName(transformationActivity.getName());
+    }
 
-  /**
-   * Fills out the Job with the username.
-   *
-   * @param username
-   *  Username to place in the job.
-   */
-  public void setUsername(String username) {
-    job.setUsername(username);
-  }
+    /**
+     * Fills out the Job with the username.
+     *
+     * @param username Username to place in the job.
+     */
+    public void setUsername(String username) {
+        job.setUsername(username);
+    }
 
-  public JobDto getJob() {
-    return job;
-  }
+    public JobDto getJob() {
+        return job;
+    }
 }
