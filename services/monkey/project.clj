6275afb1dfba1ea0de790d2c261cf7d7d9396bdb
Tony Edgin <tedgin@iplantcollaborative.org>
2014-08-07 09:37:47 -0700

    CORE-5870 created monkey project file

---
+++ services/monkey/project.clj
@@ -0,0 +1,6 @@
+(defproject org.iplantc/monkey "3.2.2"
+  :description "A metadata database crawler. It synchronizes the tag documents in the search data
+                index with the tag information inthe metadata database."
+  :url "http://www.iplantcollaborative.org"
+  :license {:name "BSD"
+            :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"})
