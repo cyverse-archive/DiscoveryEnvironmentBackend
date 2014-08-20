d9c3fc74bacdce5f81fbf5252010ee5d9759e955
Paul Sarando <psarando@iplantcollaborative.org>
2014-08-20 16:04:20 -0700

    CORE-5386 Fixed parameter.required conversion.
    Fixed the join to the file_parameters table, since the parameter.file_parameter_id conversion will not have run before this conversion.

--- databases/de-database-schema/src/main/conversions/c200_2014042401/tables/24_parameters.sql
+++ databases/de-database-schema/src/main/conversions/c200_2014042401/tables/24_parameters.sql
@@ -29,6 +29,6 @@ WITH dups AS (SELECT id, COUNT(hid_v187) FROM parameters GROUP BY id)
 UPDATE parameters SET required = true WHERE id IN
     (SELECT p.id FROM parameters p
      LEFT JOIN validator_v187 v ON v.hid = p.validator_v187
-     LEFT JOIN file_parameters f ON f.id = p.file_parameter_id
+     LEFT JOIN file_parameters f ON f.hid_v187 = p.dataobject_id_v187
      WHERE v.required = true OR f.required_v187 = true);
 
