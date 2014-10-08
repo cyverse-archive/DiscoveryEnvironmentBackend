8ff9f33e0a12ae88e345179639800dcf4cf4b764
Tony Edgin <tedgin@iplantcollaborative.org>
2014-10-08 15:12:08 -0700

    CORE-6040 cleaned up services.validators ns

--- services/data-info/src/data_info/services/validators.clj
+++ services/data-info/src/data_info/services/validators.clj
@@ -1,14 +1,12 @@
 (ns data-info.services.validators
-  (:use [clj-jargon.item-info]
-        [clj-jargon.permissions]
-        [clj-jargon.tickets]
-        [clj-jargon.users]
-        [clojure-commons.error-codes]
-        [slingshot.slingshot :only [throw+]])
+  (:use [slingshot.slingshot :only [throw+]])
   (:require [clojure.string :as str]
             [clj-icat-direct.icat :as icat]
             [clj-jargon.init :as init]
-            [data-info.util :as util]
+            [clj-jargon.item-info :as item]
+            [clj-jargon.permissions :as perm]
+            [clj-jargon.users :as user]
+            [clojure-commons.error-codes :as error]
             [data-info.util.config :as cfg]))
 
 
@@ -20,7 +18,7 @@
 (defn- validate-path-count
   [count]
   (if-not (num-paths-okay? count)
-    (throw+ {:error_code "ERR_TOO_MANY_PATHS"
+    (throw+ {:error_code error/ERR_TOO_MANY_RESULTS
              :count count
              :limit (cfg/max-paths-in-request)})))
 
@@ -43,8 +41,8 @@
 
 (defn user-exists
   ([cm user]
-   (when-not (user-exists? cm user)
-     (throw+ {:error_code ERR_NOT_A_USER :user user})))
+   (when-not (user/user-exists? cm user)
+     (throw+ {:error_code error/ERR_NOT_A_USER :user user})))
 
   ([user]
     (init/with-jargon (cfg/jargon-cfg) [cm]
@@ -53,133 +51,98 @@
 
 (defn all-users-exist
   [cm users]
-  (when-not (every? #(user-exists? cm %) users)
-    (throw+ {:error_code ERR_NOT_A_USER
-             :users (filterv #(not (user-exists? cm %1)) users)})))
+  (when-not (every? #(user/user-exists? cm %) users)
+    (throw+ {:error_code error/ERR_NOT_A_USER
+             :users      (filterv #(not (user/user-exists? cm %1)) users)})))
 
 (defn path-exists
   [cm path]
-  (when-not (exists? cm path)
-    (throw+ {:error_code ERR_DOES_NOT_EXIST
+  (when-not (item/exists? cm path)
+    (throw+ {:error_code error/ERR_DOES_NOT_EXIST
              :path path})))
 
 (defn all-paths-exist
   [cm paths]
-  (when-not (every? #(exists? cm %) paths)
-    (throw+ {:error_code ERR_DOES_NOT_EXIST
-             :paths (filterv #(not (exists? cm  %1)) paths)})))
+  (when-not (every? #(item/exists? cm %) paths)
+    (throw+ {:error_code error/ERR_DOES_NOT_EXIST
+             :paths      (filterv #(not (item/exists? cm  %1)) paths)})))
 
 (defn no-paths-exist
   [cm paths]
-  (when (some #(exists? cm %) paths)
-    (throw+ {:error_code ERR_EXISTS
-             :paths (filterv #(exists? cm %) paths)})))
+  (when (some #(item/exists? cm %) paths)
+    (throw+ {:error_code error/ERR_EXISTS
+             :paths (filterv #(item/exists? cm %) paths)})))
 
 (defn path-readable
   [cm user path]
-  (when-not (is-readable? cm user path)
-    (throw+ {:error_code ERR_NOT_READABLE
+  (when-not (perm/is-readable? cm user path)
+    (throw+ {:error_code error/ERR_NOT_READABLE
              :path path
              :user user})))
 
 (defn all-paths-readable
   [cm user paths]
-  (when-not (every? #(is-readable? cm user %) paths)
-    (throw+ {:error_code ERR_NOT_READABLE
-             :path (filterv #(not (is-readable? cm user %)) paths)})))
+  (when-not (every? #(perm/is-readable? cm user %) paths)
+    (throw+ {:error_code error/ERR_NOT_READABLE
+             :path       (filterv #(not (perm/is-readable? cm user %)) paths)})))
 
 (defn path-writeable
   [cm user path]
-  (when-not (is-writeable? cm user path)
-    (throw+ {:error_code ERR_NOT_WRITEABLE
+  (when-not (perm/is-writeable? cm user path)
+    (throw+ {:error_code error/ERR_NOT_WRITEABLE
              :path path})))
 
 (defn all-paths-writeable
   [cm user paths]
-  (when-not (paths-writeable? cm user paths)
-    (throw+ {:paths (filterv #(not (is-writeable? cm user %)) paths)
-             :error_code ERR_NOT_WRITEABLE})))
+  (when-not (perm/paths-writeable? cm user paths)
+    (throw+ {:paths      (filterv #(not (perm/is-writeable? cm user %)) paths)
+             :error_code error/ERR_NOT_WRITEABLE})))
 
 (defn path-not-exists
   [cm path]
-  (when (exists? cm path)
+  (when (item/exists? cm path)
     (throw+ {:path path
-             :error_code ERR_EXISTS})))
+             :error_code error/ERR_EXISTS})))
 
 (defn path-is-dir
   [cm path]
-  (when-not (is-dir? cm path)
-    (throw+ {:error_code ERR_NOT_A_FOLDER
+  (when-not (item/is-dir? cm path)
+    (throw+ {:error_code error/ERR_NOT_A_FOLDER
              :path path})))
 
 (defn path-is-file
   [cm path]
-  (when-not (is-file? cm path)
-    (throw+ {:error_code ERR_NOT_A_FILE
+  (when-not (item/is-file? cm path)
+    (throw+ {:error_code error/ERR_NOT_A_FILE
              :path path})))
 
 (defn paths-are-files
   [cm paths]
-  (when-not (every? #(is-file? cm %) paths)
-    (throw+ {:error_code ERR_NOT_A_FILE
-             :path (filterv #(not (is-file? cm %)) paths)})))
+  (when-not (every? #(item/is-file? cm %) paths)
+    (throw+ {:error_code error/ERR_NOT_A_FILE
+             :path       (filterv #(not (item/is-file? cm %)) paths)})))
 
-(defn path-satisfies-predicate
-  [cm path pred-func? pred-err]
-  (when-not (pred-func? cm  path)
-    (throw+ {:paths path
-             :error_code pred-err})))
-
-(defn paths-satisfy-predicate
-  [cm paths pred-func? pred-err]
-  (when-not  (every? true? (mapv #(pred-func? cm %) paths))
-    (throw+ {:error_code pred-err
-             :paths (filterv #(not (pred-func? cm %)) paths)})))
-
-(defn ownage?
-  [cm user path]
-  (owns? cm user path))
 
 (defn user-owns-path
   [cm user path]
-  (when-not (owns? cm user path)
-    (throw+ {:error_code ERR_NOT_OWNER
+  (when-not (perm/owns? cm user path)
+    (throw+ {:error_code error/ERR_NOT_OWNER
              :user user
              :path path})))
+
+
+(defn- ownage?
+  [cm user path]
+  (perm/owns? cm user path))
+
 
 (defn user-owns-paths
   [cm user paths]
   (let [belongs-to? (partial ownage? cm user)]
     (when-not (every? #(belongs-to? %) paths)
-      (throw+ {:error_code ERR_NOT_OWNER
+      (throw+ {:error_code error/ERR_NOT_OWNER
                :user user
                :paths (filterv #(not (belongs-to? %)) paths)}))))
-
-(defn ticket-exists
-  [cm user ticket-id]
-  (when-not (ticket? cm (:username cm) ticket-id)
-    (throw+ {:error_code ERR_TICKET_DOES_NOT_EXIST
-             :user user
-             :ticket-id ticket-id})))
-
-(defn ticket-does-not-exist
-  [cm user ticket-id]
-  (when (ticket? cm (:username cm) ticket-id)
-    (throw+ {:error_code ERR_TICKET_EXISTS
-             :user user
-             :ticket-id ticket-id})))
-
-(defn all-tickets-exist
-  [cm user ticket-ids]
-  (when-not (every? #(ticket? cm (:username cm) %) ticket-ids)
-    (throw+ {:ticket-ids (filterv #(not (ticket? cm (:username cm) %)) ticket-ids)
-             :error_code ERR_TICKET_DOES_NOT_EXIST})))
-
-(defn all-tickets-nonexistant
-  [cm user ticket-ids]
-  (when (some #(ticket? cm (:username cm) %) ticket-ids)
-    (throw+ {:ticket-ids (filterv #(ticket? cm (:username cm) %) ticket-ids)
-             :error_code ERR_TICKET_EXISTS})))
 
 
 (defn valid-bool-param
@@ -198,6 +161,6 @@
   [^String param-name ^String param-val]
   (let [val (str/lower-case param-val)]
     (when-not (or (= val "true") (= val "false"))
-      (throw+ {:error_code ERR_BAD_REQUEST
+      (throw+ {:error_code error/ERR_BAD_REQUEST
                :param      param-name
                :value      param-val}))))
