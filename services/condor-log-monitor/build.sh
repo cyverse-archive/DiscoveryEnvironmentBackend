fcde3dc72909a0dd3b00862c3f09378b4c243ff9
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-10-13 14:09:28 -0700

    Added the correct ownership for the file in the condor-log-monitor rpm.

--- services/condor-log-monitor/build.sh
+++ services/condor-log-monitor/build.sh
@@ -7,10 +7,12 @@ echo "WORKINGIDR=$WORKINGDIR"
 
 cd $WORKINGDIR
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
+USER=condor
+GROUP=condor
 git checkout $BRANCH
 mkdir -p build/usr/local/bin
 mkdir -p build/var/log/condor-log-monitor
 godep restore
 go build .
 cp condor-log-monitor build/usr/local/bin/
-fpm -s dir -t rpm --directories /var/log/condor-log-monitor --version $VERSION --epoch 0 --prefix / --name condor-log-monitor --verbose -C build -f .
+fpm -s dir -t rpm --directories /var/log/condor-log-monitor --version $VERSION --epoch 0 --prefix / --name condor-log-monitor --verbose -C build --rpm-user $USER --rpm-group $GROUP -f .
