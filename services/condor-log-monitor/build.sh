6d66c242c52c63452a8dfcadea74e535cbd16fad
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-10-13 14:53:09 -0700

    Added the iteration to the build.sh file for condor-log-monitor.

--- services/condor-log-monitor/build.sh
+++ services/condor-log-monitor/build.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
-BRANCH=$1
+ITERATION=$1
 WORKINGDIR=$2
-echo "BRANCH=$BRANCH"
 echo "WORKINGIDR=$WORKINGDIR"
 USER=condor
 GROUP=condor
 
 cd $WORKINGDIR
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
-git checkout $BRANCH
 mkdir -p build/usr/local/bin
 mkdir -p build/var/log/condor-log-monitor
 godep restore
 go build .
 cp condor-log-monitor build/usr/local/bin/
-fpm -s dir -t rpm --directories /var/log/condor-log-monitor --version $VERSION --epoch 0 --prefix / --name condor-log-monitor --verbose -C build --rpm-user $USER --rpm-group $GROUP -f .
+fpm -s dir -t rpm --directories /var/log/condor-log-monitor --version $VERSION --iteration $ITERATION --epoch 0 --prefix / --name condor-log-monitor --verbose -C build --rpm-user $USER --rpm-group $GROUP -f .
