2aa770fcf315a506e4b371d9501786e5ff8e0c78
Paul Sarando <psarando@iplantcollaborative.org>
2014-10-14 10:06:57 -0700

    Bumped versions to 4.0.1

--- libs/clj-icat-direct/project.clj
+++ libs/clj-icat-direct/project.clj
@@ -1,4 +1,4 @@
-(defproject org.iplantc/clj-icat-direct "3.2.9"
+(defproject org.iplantc/clj-icat-direct "4.0.1"
   :description "A Clojure library for accessing the iRODS ICAT database directly."
   :url "http://github.com/iPlantCollaborativeOpenSource/clj-icat-direct/"
   :license {:name "BSD Standard License"
--- tools/facepalm/project.clj
+++ tools/facepalm/project.clj
@@ -12,7 +12,7 @@
       ""))
 
 
-(defproject org.iplantc/facepalm "3.2.9"
+(defproject org.iplantc/facepalm "4.0.1"
   :description "Command-line utility for DE database managment."
   :url "http://www.iplantcollaborative.org"
   :license {:name "BSD"
@@ -28,14 +28,14 @@
                  [fleet "0.10.1"]
                  [korma "0.4.0"]
                  [me.raynes/fs "1.4.6"]
-                 [org.iplantc/clj-jargon "3.2.9"]
-                 [org.iplantc/clojure-commons "3.2.9"]
-                 [org.iplantc/kameleon "3.2.9"]
+                 [org.iplantc/clj-jargon "4.0.1"]
+                 [org.iplantc/clojure-commons "4.0.1"]
+                 [org.iplantc/kameleon "4.0.1"]
                  [postgresql "9.1-901-1.jdbc4"]
                  [slingshot "0.10.3"]
                  [clj-http "1.0.0"]]
-  :plugins [[org.iplantc/lein-iplant-cmdtar "3.2.9"]
-            [org.iplantc/lein-iplant-rpm "3.2.9"]
+  :plugins [[org.iplantc/lein-iplant-cmdtar "4.0.1"]
+            [org.iplantc/lein-iplant-rpm "4.0.1"]
             [lein-marginalia "0.7.1"]]
   :iplant-rpm {:summary "Facepalm"
                :type :command}
