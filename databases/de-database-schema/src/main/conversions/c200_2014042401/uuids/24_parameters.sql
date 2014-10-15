b743c20544bbfd12e6057cce274f2121dfdd276e
Dennis Roberts <dennis@iplantcollaborative.org>
2014-10-14 19:45:41 -0700

    CORE-5386: added columns to the input_output_mappings table to support inputs and outputs in external apps

--- databases/de-database-schema/src/main/conversions/c200_2014042401/uuids/24_parameters.sql
+++ databases/de-database-schema/src/main/conversions/c200_2014042401/uuids/24_parameters.sql
@@ -18,14 +18,19 @@ UPDATE validation_rules r SET parameter_id =
      LEFT JOIN validator_rule_v187 vr ON vr.validator_id = p.validator_v187
      WHERE r.hid_v187 = vr.rule_id);
 
-UPDATE input_output_mapping SET input =
-    (SELECT p.id FROM parameters p
-     LEFT JOIN file_parameters f ON f.parameter_id = p.id
-     WHERE f.id_v187 = input_v187);
-UPDATE input_output_mapping SET output =
-    (SELECT p.id FROM parameters p
-     LEFT JOIN file_parameters f ON f.parameter_id = p.id
-     WHERE f.id_v187 = output_v187);
+-- Copy input/output mappings for DE parameters to the new fields.
+UPDATE input_output_mapping iom SET input = p.id
+    FROM parameters p JOIN file_parameters f ON f.parameter_id = p.id
+    WHERE f.id_v187 = input_v187;
+UPDATE input_output_mapping iom SET output = p.id
+    FROM parameters P JOIN file_parameters f ON f.parameter_id = p.id
+    WHERE f.id_v187 = output_v187;
+
+-- Copy input/output mappings for external parameters to the new fields.
+UPDATE input_output_mapping SET external_input = input_v187
+    WHERE input IS NULL;
+UPDATE input_output_mapping SET external_output = output_v187
+    WHERE output IS NULL;
 
 -- Drop temporary indexes.
 DROP INDEX parameters_validator_idx;
@@ -37,11 +42,6 @@ ALTER TABLE validation_rule_arguments DROP CONSTRAINT rule_argument_rule_id_fkey
 ALTER TABLE validator_rule_v187 DROP CONSTRAINT validator_rule_rule_id_fkey;
 DELETE FROM validation_rules WHERE parameter_id IS NULL;
 
-DELETE FROM input_output_mapping WHERE input IS NULL OR output IS NULL;
-
--- Add NOT NULL constraints on foreign key columns.
+-- Add NOT NULL constraints on required foreign key columns.
 ALTER TABLE ONLY file_parameters ALTER COLUMN parameter_id SET NOT NULL;
 ALTER TABLE ONLY validation_rules ALTER COLUMN parameter_id SET NOT NULL;
-ALTER TABLE ONLY input_output_mapping ALTER COLUMN input SET NOT NULL;
-ALTER TABLE ONLY input_output_mapping ALTER COLUMN output SET NOT NULL;
-
--- databases/de-database-schema/src/main/tables/18_input_output_mapping.sql
+++ databases/de-database-schema/src/main/tables/18_input_output_mapping.sql
@@ -5,6 +5,8 @@ SET search_path = public, pg_catalog;
 --
 CREATE TABLE input_output_mapping (
     mapping_id uuid NOT NULL,
-    input uuid NOT NULL,
-    output uuid NOT NULL
+    input uuid,
+    external_input character varying(255),
+    output uuid,
+    external_output character varying(255)
 );
