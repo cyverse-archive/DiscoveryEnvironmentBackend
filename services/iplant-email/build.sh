e3384f7c67fde101cd1a9b62265fd95509678c3f
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-10-15 14:12:14 -0700

    Added build.sh and version for iplant-email.

---
+++ services/iplant-email/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -x
+
+ITERATION=$1
+USER=iplant
+GROUP=iplant
+BINNAME=iplant-email
+BUILDDIR=$BINNAME-build
+BINDIR=/usr/local/lib/$BINNAME
+LOGDIR=/var/log/$BINNAME
+
+VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
+if [ -d "$BUILDDIR" ]; then
+  rm -r $BUILDDIR
+fi
+mkdir -p $BUILDDIR/$BINDIR
+mkdir -p $BUILDDIR/$LOGDIR
+lein clean
+lein deps
+lein uberjar
+cp target/$BINNAME-*-standalone.jar $BUILDDIR/$BINDIR
+fpm -s dir -t rpm --directories $LOGDIR -d java-1.7.0-openjdk --version $VERSION --iteration $ITERATION --epoch 0 --prefix / --name $BINNAME --verbose -C $BUILDDIR --rpm-user $USER --rpm-group $GROUP -f .
