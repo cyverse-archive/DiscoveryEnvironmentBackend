62323a23ec00e34c361cf705bb6d6ed25d5a28a7
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-10-15 08:36:46 -0700

    Fixed up a bunch of errors in the build.sh script for condor-log-monitor.

--- services/condor-log-monitor/build.sh
+++ services/condor-log-monitor/build.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
+set -x
 
 ITERATION=$1
 USER=condor
 GROUP=condor
 
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
-if [ -d build ]; then
-  rm -r build
+if [ -d "build" ]; then
+  rm -r clm-build
 fi
-mkdir -p build/usr/local/bin
-mkdir -p build/var/log/condor-log-monitor
+mkdir -p clm-build/usr/local/bin
+mkdir -p clm-build/var/log/condor-log-monitor
 godep restore
 go build .
-cp condor-log-monitor build/usr/local/bin/
-fpm -s dir -t rpm --directories /var/log/condor-log-monitor --version $VERSION --iteration $ITERATION --epoch 0 --prefix / --name condor-log-monitor --verbose -C build --rpm-user $USER --rpm-group $GROUP -f .
+cp condor-log-monitor clm-build/usr/local/bin/
+fpm -s dir -t rpm --directories /var/log/condor-log-monitor --version $VERSION --iteration $ITERATION --epoch 0 --prefix / --name condor-log-monitor --verbose -C clm-build --rpm-user $USER --rpm-group $GROUP -f .
