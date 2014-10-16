964000883d927edea93ca663a1a8f52090d0081b
tedgin <tedgin@iplantcollaborative.org>
2014-10-16 14:30:37 -0700

    CORE-6040 simplified response handling

--- services/data-info/src/data_info/util.clj
+++ services/data-info/src/data_info/util.clj
@@ -1,23 +1,18 @@
 (ns data-info.util
   "Utility functions for defining services in data-info. This namespace is used by data-info.core
    and several other top-level service definition namespaces."
-  (:use [compojure.core]
-        [slingshot.slingshot :only [try+ throw+]])
-  (:require [clojure-commons.error-codes :as ce]
-            [clojure.tools.logging :as log]
+  (:require [clojure.tools.logging :as log]
+            [slingshot.slingshot :refer [try+]]
+            [clojure-commons.error-codes :as ce]
             [data-info.util.service :as svc]
             [data-info.util.transformers :as transform]))
 
 
-(defn determine-response
+(defn- determine-response
   [resp-val]
   (if (and (map? resp-val) (number? (:status resp-val)))
     (svc/data-info-response resp-val (:status resp-val))
     (svc/success-response resp-val)))
-
-(defn clj-http-error?
-  [{:keys [status body]}]
-  (and (number? status) ((comp not nil?) body)))
 
 
 (defn trap
@@ -25,20 +20,11 @@
   [f]
   (try+
     (determine-response (f))
-    (catch [:type :error-status] {:keys [res]} res)
-    (catch [:type :missing-argument] {:keys [arg]} (svc/missing-arg-response arg))
-    (catch [:type :invalid-argument] {:keys [arg val reason]}
-      (svc/invalid-arg-response arg val reason))
-    (catch [:type :invalid-configuration] {:keys [reason]} (svc/invalid-cfg-response reason))
-    (catch [:type :temp-dir-failure] err (svc/temp-dir-failure-response err))
     (catch [:error_code ce/ERR_NOT_A_USER] err (svc/failure-response err))
     (catch ce/error? err
-      (log/error (ce/format-exception (:throwable &throw-context)))
+      (when (>= 500 (ce/get-http-status (:error_code err)))
+        (log/error (ce/format-exception (:throwable &throw-context))))
       (svc/error-response err (ce/get-http-status (:error_code err))))
-    (catch IllegalArgumentException e (svc/failure-response e))
-    (catch IllegalStateException e (svc/failure-response e))
-    (catch Throwable t (svc/error-response t))
-    (catch clj-http-error? o o)
     (catch Object o (svc/error-response (Exception. (str "unexpected error: " o))))))
 
 
@@ -47,24 +33,12 @@
   (fn [req]
     (trap #(handler req))))
 
+
 (defn req-logger
   [handler]
   (fn [req]
     (log/info "Request received:" req)
     (handler req)))
-
-(defn as-vector
-  "Returns the given parameter inside a vector if it's not a vector already."
-  [p]
-  (cond (nil? p)    []
-        (vector? p) p
-        :else       [p]))
-
-(defn optional-routes
-  "Creates a set of optionally defined routes."
-  [[option-fn] & handlers]
-  (when (option-fn)
-    (apply routes handlers)))
 
 
 (defn- pre-process-request
@@ -80,6 +54,7 @@
         get-arg (fn [arg] (if (keyword? arg) (get req arg) arg))
         argv    (mapv get-arg args)]
     (trap #(apply func argv))))
+
 
 (defn controller
   [req func & args]
