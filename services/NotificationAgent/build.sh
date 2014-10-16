453b3ed6759bd22118690d64975ebb9113d50f4e
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-10-16 08:49:09 -0700

    Added conf files to build.sh for notificationagent.

--- services/NotificationAgent/build.sh
+++ services/NotificationAgent/build.sh
@@ -8,6 +8,8 @@ BINNAME=notificationagent
 BUILDDIR=notificationagent
 BINDIR=/usr/local/lib/notificationagent
 LOGDIR=/var/log/notificationagent
+CONFDIR=/etc/notificationagent
+REPODIR=conf/main
 
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
 if [ -d "$BUILDDIR" ]; then
@@ -15,8 +17,10 @@ if [ -d "$BUILDDIR" ]; then
 fi
 mkdir -p $BUILDDIR/$BINDIR
 mkdir -p $BUILDDIR/$LOGDIR
+mkdir -p $BUILDDIR/$CONFDIR
 lein clean
 lein deps
 lein uberjar
 cp target/$BINNAME-*-standalone.jar $BUILDDIR/$BINDIR
+cp $REPODIR/* $BUILDDIR/$CONFDIR
 fpm -s dir -t rpm --directories $LOGDIR -d java-1.7.0-openjdk --version $VERSION --iteration $ITERATION --epoch 0 --prefix / --name $BINNAME --verbose -C $BUILDDIR --rpm-user $USER --rpm-group $GROUP -f .
