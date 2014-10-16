a721d08b0c5f8c216510fff1ae1df00b524041aa
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-10-16 09:09:39 -0700

    Added the conf files to build.sh for data-info.

--- services/data-info/build.sh
+++ services/data-info/build.sh
@@ -8,6 +8,8 @@ BINNAME=data-info
 BUILDDIR=$BINNAME-build
 BINDIR=/usr/local/lib/$BINNAME
 LOGDIR=/var/log/$BINNAME
+CONFDIR=/etc/data-info
+REPODIR=conf/main
 
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
 if [ -d "$BUILDDIR" ]; then
@@ -15,8 +17,10 @@ if [ -d "$BUILDDIR" ]; then
 fi
 mkdir -p $BUILDDIR/$BINDIR
 mkdir -p $BUILDDIR/$LOGDIR
+mkdir -p $BUILDDIR/$CONFDIR
 lein clean
 lein deps
 lein uberjar
 cp target/$BINNAME-*-standalone.jar $BUILDDIR/$BINDIR
+cp $REPODIR/* $BUILDDIR/$CONFDIR
 fpm -s dir -t rpm --directories $LOGDIR -d java-1.7.0-openjdk --version $VERSION --iteration $ITERATION --epoch 0 --prefix / --name $BINNAME --verbose -C $BUILDDIR --rpm-user $USER --rpm-group $GROUP -f .
