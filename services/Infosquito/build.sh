dcf5fd0b5adc9b156f5283a29d8e0f28165c274f
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-10-16 08:45:33 -0700

    Added conf files to build.sh for Infosquito.

--- services/Infosquito/build.sh
+++ services/Infosquito/build.sh
@@ -8,6 +8,8 @@ BINNAME=infosquito
 BUILDDIR=infosquito-build
 BINDIR=/usr/local/lib/infosquito
 LOGDIR=/var/log/infosquito
+CONFDIR=/etc/infosquito
+REPODIR=conf
 
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
 if [ -d "$BUILDDIR" ]; then
@@ -15,8 +17,10 @@ if [ -d "$BUILDDIR" ]; then
 fi
 mkdir -p $BUILDDIR/$BINDIR
 mkdir -p $BUILDDIR/$LOGDIR
+mkdir -p $BUILDDIR/$CONFDIR
 lein clean
 lein deps
 lein uberjar
 cp target/$BINNAME-*-standalone.jar $BUILDDIR/$BINDIR
+cp $REPODIR/* $BUILDDIR/$CONFDIR
 fpm -s dir -t rpm --directories $LOGDIR -d java-1.7.0-openjdk --version $VERSION --iteration $ITERATION --epoch 0 --prefix / --name $BINNAME --verbose -C $BUILDDIR --rpm-user $USER --rpm-group $GROUP -f .
