0eed2953ad87e665e9bb014df2334747b85e0eb2
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-10-16 13:56:58 -0700

    Fixed the build.sh file for facepalm.

--- tools/facepalm/build.sh
+++ tools/facepalm/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -x
+set -e
 
 ITERATION=$1
 USER=iplant
@@ -18,5 +19,5 @@ mkdir -p $BUILDDIR/$LOGDIR
 lein clean
 lein deps
 lein uberjar
-cp target/$BINNAME-standalone.jar $BUILDDIR/$BINDIR
+cp target/$BINNAME-*-standalone.jar $BUILDDIR/$BINDIR
 fpm -s dir -t rpm --directories $LOGDIR -d java-1.7.0-openjdk --version $VERSION --iteration $ITERATION --epoch 0 --prefix / --name $BINNAME --verbose -C $BUILDDIR --rpm-user $USER --rpm-group $GROUP -f .
