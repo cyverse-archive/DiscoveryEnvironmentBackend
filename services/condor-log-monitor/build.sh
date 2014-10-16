c481827674360c8d7074af35b40cde56de47aec9
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-10-16 13:29:39 -0700

    Removed the debug code. PEBCAK.

--- services/condor-log-monitor/build.sh
+++ services/condor-log-monitor/build.sh
@@ -14,6 +14,5 @@ mkdir -p clm-build/usr/local/bin
 mkdir -p clm-build/var/log/condor-log-monitor
 godep restore
 go build .
-ls -lR build
 cp condor-log-monitor clm-build/usr/local/bin/
 fpm -s dir -t rpm --directories /var/log/condor-log-monitor --version $VERSION --iteration $ITERATION --epoch 0 --prefix / --name condor-log-monitor --verbose -C clm-build --rpm-user $USER --rpm-group $GROUP -f .
