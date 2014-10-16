1dbd3fe45c032edf49be78fa38f0477b64699d2e
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-10-16 09:13:06 -0700

    Added conf files to build.sh for iplant-email.

--- services/iplant-email/build.sh
+++ services/iplant-email/build.sh
@@ -8,6 +8,8 @@ BINNAME=iplant-email
 BUILDDIR=$BINNAME-build
 BINDIR=/usr/local/lib/$BINNAME
 LOGDIR=/var/log/$BINNAME
+CONFDIR=/etc/iplant-email
+REPODIR=conf
 
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
 if [ -d "$BUILDDIR" ]; then
@@ -15,8 +17,10 @@ if [ -d "$BUILDDIR" ]; then
 fi
 mkdir -p $BUILDDIR/$BINDIR
 mkdir -p $BUILDDIR/$LOGDIR
+mkdir -p $BUILDDIR/$CONFDIR
 lein clean
 lein deps
 lein uberjar
 cp target/$BINNAME-*-standalone.jar $BUILDDIR/$BINDIR
+cp $REPODIR/* $BUILDDIR/$CONFDIR
 fpm -s dir -t rpm --directories $LOGDIR -d java-1.7.0-openjdk --version $VERSION --iteration $ITERATION --epoch 0 --prefix / --name $BINNAME --verbose -C $BUILDDIR --rpm-user $USER --rpm-group $GROUP -f .
