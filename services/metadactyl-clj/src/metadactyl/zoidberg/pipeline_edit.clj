cyverse-archive/DiscoveryEnvironmentBackend

commit 6d4f040f25c5dc9a14e158d6a979f06f9f2c34fb

CORE-5832 Removed IDs from Pipeline JSON steps.
Pipeline endpoints now use the JSON array index to refer to source and target steps in the mapping JSON, so the client is no longer required to generate UUIDs for new steps.
The step UUIDs have also been removed from the step JSON objects, since they are no longer used in the mapping JSON.

services/metadactyl-clj/src/metadactyl/zoidberg/pipeline_edit.clj
@@ -97,6 +97,7 @@
             (join [:apps :app]
                   {:app_id :app.id})
             (fields :app_steps.id
+                    :step
                     :t.name
                     :t.description
                     :task_id
@@ -107,14 +108,7 @@
 (defn- format-step
   "Formats step fields for the client."
   [step]
-  (dissoc step :input_mapping))
-
-(defn- format-step-copy
-  "Formats step fields as copies for an update-workflow call."
-  [new-step-ids step]
-  (-> step
-      (dissoc :input_mapping)
-      (assoc :id (new-step-ids (:id step)))))
+  (dissoc step :id :step :input_mapping))
 
 (defn- get-input-output-mappings
   "Fetches the output->input mapping UUIDs for the given source and target IDs."
@@ -129,34 +123,29 @@
 
 (defn- format-mapping
   "Formats mapping fields for the client."
-  [mapping]
-  (let [input-output-mappings (get-input-output-mappings (:source_step mapping)
-                                                         (:target_step mapping))
+  [step-indexes {source-step-id :source_step target-step-id :target_step}]
+  (let [input-output-mappings (get-input-output-mappings source-step-id target-step-id)
         input-output-reducer #(assoc %1 (:input %2) (:output %2))]
-    (assoc mapping :map (reduce input-output-reducer {} input-output-mappings))))
+    {:source_step (step-indexes source-step-id)
+     :target_step (step-indexes target-step-id)
+     :map (reduce input-output-reducer {} input-output-mappings)}))
 
 (defn- get-formatted-mapping
   "Formats a step's list of mapping IDs and step names to fields for the client."
-  [step]
-  (map format-mapping (:input_mapping step)))
+  [step-indexes step]
+  (map (partial format-mapping step-indexes) (:input_mapping step)))
 
-(defn- format-mapping-new-ids
-  [new-step-ids mapping]
-  (assoc mapping
-    :source_step (new-step-ids (:source_step mapping))
-    :target_step (new-step-ids (:target_step mapping))))
-
-(defn- get-formatted-mapping-copy
-  "Formats a step's list of mapping IDs and step names to fields for the client."
-  [new-step-ids step]
-  (map (comp (partial format-mapping-new-ids new-step-ids) format-mapping)
-       (:input_mapping step)))
+(defn- get-mappings
+  "Gets a list of formatted mappings for the given steps."
+  [steps]
+  (let [step-indexes (into {} (map #(vector (:id %) (:step %)) steps))]
+    (mapcat (partial get-formatted-mapping step-indexes) steps)))
 
 (defn- format-workflow-app
   "Adds the steps and mappings fields to the app."
   [app]
   (let [steps (get-steps (:id app))
-        mappings (mapcat get-formatted-mapping steps)
+        mappings (get-mappings steps)
         steps (map format-step steps)]
     (-> app
         (assoc :steps steps)
@@ -178,9 +167,8 @@
    appropriate app fields to prepare it for saving as a copy."
   [app]
   (let [steps (get-steps (:id app))
-        new-step-ids (into {} (map #(vector (:id %) (uuid)) steps))
-        mappings (mapcat (partial get-formatted-mapping-copy new-step-ids) steps)
-        steps (map (partial format-step-copy new-step-ids) steps)]
+        mappings (get-mappings steps)
+        steps (map format-step steps)]
     (-> app
         (select-keys [:description])
         (assoc :name (app-copy-name (:name app)))
@@ -194,6 +182,18 @@
     (verify-app-editable app)
     (service/success-response (format-workflow app))))
 
+(defn- add-app-mapping
+  [app-id steps {:keys [source_step target_step map]}]
+  (add-mapping {:app_id app-id
+                :source_step (:id (nth steps source_step))
+                :target_step (:id (nth steps target_step))
+                :map map}))
+
+(defn- add-app-steps-mappings
+  [{app-id :id steps :steps mappings :mappings}]
+  (let [steps (map-indexed (partial add-step app-id) steps)]
+    (dorun (map (partial add-app-mapping app-id steps) mappings))))
+
 (defn- add-pipeline-app
   [app]
   (transaction
@@ -201,8 +201,7 @@
           workspace-category-id (:root_category_id (get-workspace))
           dev-group-id (get-app-subcategory-id workspace-category-id (workspace-dev-app-group-index))]
       (add-app-to-group dev-group-id app-id)
-      (dorun (map-indexed (partial add-step app-id) (:steps app)))
-      (dorun (map (partial add-mapping app-id) (:mappings app)))
+      (add-app-steps-mappings app)
       app-id)))
 
 (defn- update-pipeline-app
@@ -212,8 +211,7 @@
       (verify-app-editable (get-app app-id))
       (update-app app)
       (remove-app-steps app-id)
-      (dorun (map-indexed (partial add-step app-id) (:steps app)))
-      (dorun (map (partial add-mapping app-id) (:mappings app)))
+      (add-app-steps-mappings app)
       app-id)))
 
 (defn add-pipeline

services/metadactyl-clj/src/metadactyl/persistence/app_metadata.clj
@@ -44,18 +44,15 @@
   "Adds an app step to the database for the given app ID."
   [app-id step-number step]
   (let [step (-> step
-                 (select-keys [:id :task_id])
+                 (select-keys [:task_id])
                  (assoc :app_id app-id
                         :step step-number))]
     (insert app_steps (values step))))
 
 (defn add-mapping
-  "Adds an input/output workflow mapping to the database for the given app ID and source->target
-   mapping."
-  [app-id mapping]
-  (let [workflow-map (-> mapping
-                         (select-keys [:source_step :target_step])
-                         (assoc :app_id app-id))
+  "Adds an input/output workflow mapping to the database for the given app source->target mapping."
+  [mapping]
+  (let [workflow-map (select-keys mapping [:app_id :source_step :target_step])
         mapping-id (:id (insert :workflow_io_maps (values workflow-map)))]
     (dorun
       (for [[input output] (:map mapping)]

services/metadactyl-clj/src/metadactyl/routes/domain/pipeline.clj
@@ -5,14 +5,13 @@
   (:import [java.util UUID]))
 
 (defschema PipelineMapping
-  {:target_step (describe UUID "A UUID that is used to identify the Target Step")
-   :source_step (describe UUID "A UUID that is used to identify the Source Step")
+  {:source_step (describe Long "The step index of the Source Step")
+   :target_step (describe Long "The step index of the Target Step")
    ;; KLUDGE
    :map (describe Any "The {'input-uuid': 'output-uuid'} mapping")})
 
 (defschema PipelineStep
-  {:id          (describe UUID "A UUID that is used to identify the Step")
-   :name        (describe String "The Step's name")
+  {:name        (describe String "The Step's name")
    :description (describe String "The Step's description")
    :task_id     (describe UUID "A UUID that is used to identify this Step's Task")
    :app_type    (describe String "The Step's App type")})

commit efd9d7bc250f75dc0a68a66024a33ad1949d5016

CORE-5832 Fixed pipeline create, copy, and edit endpoints.
Fixes for bugs introduced by commits 6d4f040f25c5dc9a14e158d6a979f06f9f2c34fb and 47b9cf431a799939232e762446d4133841d5a14f

services/metadactyl-clj/src/metadactyl/zoidberg/pipeline_edit.clj
@@ -125,6 +125,7 @@
         mappings (get-mappings steps)
         steps (map format-step steps)]
     (-> app
+        (select-keys [:id :name :description])
         (assoc :steps steps)
         (assoc :mappings mappings)
         (dissoc :integrator_email
@@ -176,7 +177,7 @@
   (transaction
     (let [app-id (:id (add-app app))]
       (add-app-to-user-dev-category app-id)
-      (add-app-steps-mappings app)
+      (add-app-steps-mappings (assoc app :id app-id))
       app-id)))
 
 (defn- update-pipeline-app