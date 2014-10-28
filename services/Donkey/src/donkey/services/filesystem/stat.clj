9dba63c74dbd7be6f47d477705860ff946a70ded
Tony Edgin <tedgin@iplantcollaborative.org>
2014-10-28 10:10:59 -0700

    CORE-6040 donkey uses data-info stat gatherer
    Donkey's /secured/filesystem/stat endpoint now uses data-info's /stat-gatherer endpoint.

--- services/Donkey/src/donkey/services/filesystem/stat.clj
+++ services/Donkey/src/donkey/services/filesystem/stat.clj
@@ -1,17 +1,16 @@
 (ns donkey.services.filesystem.stat
-  (:use [clojure-commons.error-codes]
-        [clojure-commons.validators]
-        [donkey.services.filesystem.validators]
+  (:use [clojure-commons.validators]
         [clj-jargon.init :only [with-jargon]]
         [clj-jargon.item-info :only [is-dir? stat]]
         [clj-jargon.item-ops :only [input-stream]]
         [clj-jargon.metadata :only [get-attribute]]
-        [clj-jargon.permissions :only [list-user-perms permission-for owns?]]
-        [slingshot.slingshot :only [try+ throw+]])
+        [clj-jargon.permissions :only [list-user-perms permission-for owns?]])
   (:require [clojure.tools.logging :as log]
             [clojure.string :as string]
+            [cemerick.url :as url]
             [clojure-commons.file-utils :as ft]
             [cheshire.core :as json]
+            [clj-http.client :as http]
             [dire.core :refer [with-pre-hook! with-post-hook!]]
             [donkey.services.filesystem.validators :as validators]
             [donkey.services.filesystem.garnish.irods :as filetypes]
@@ -83,10 +82,14 @@
     (validators/path-exists cm path)
     (decorate-stat cm user (stat cm path))))
 
+
 (defn do-stat
-  [{user :user} {paths :paths}]
-  (with-jargon (jargon/jargon-cfg) [cm]
-    {:paths (into {} (map #(vector % (path-stat cm user %)) paths))}))
+  [params body]
+  (let [url     (url/url (cfg/data-info-base-url) "stat-gatherer")
+        req-map {:query-params params
+                 :content-type :json
+                 :body         (json/encode body)}]
+    (:body (http/post (str url) req-map))))
 
 (with-pre-hook! #'do-stat
   (fn [params body]
@@ -95,6 +98,6 @@
     (validate-map body {:paths vector?})
     (validate-map body {:paths #(not (empty? %1))})
     (validate-map body {:paths #(every? (comp not string/blank?) %1)})
-    (validate-num-paths (:paths body))))
+    (validators/validate-num-paths (:paths body))))
 
 (with-post-hook! #'do-stat (paths/log-func "do-stat"))
