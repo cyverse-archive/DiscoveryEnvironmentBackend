7e157c2399e0e89502aa42cf4b2f6784f4cad009
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-11-05 13:52:26 -0700

    Made sure that the type of the event_number field is correct in the table def for condor_events.

--- databases/jex-db/src/main/tables/02_condor_events.sql
+++ databases/jex-db/src/main/tables/02_condor_events.sql
@@ -5,7 +5,7 @@ SET search_path = public, pg_catalog;
 --
 CREATE TABLE condor_events (
   id           uuid not null default uuid_generate_v1(), -- primary key
-  event_number integer not null,
+  event_number varchar(3) not null,
   event_name   text not null,
   event_desc   text not null
 );
