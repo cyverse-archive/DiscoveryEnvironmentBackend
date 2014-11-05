524ff74d59c0f56573e4fc25207fe5bd0ed7b931
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-11-05 15:41:01 -0700

    Added build.sh for jex-events.

--- services/jex-events/build.sh
+++ services/jex-events/build.sh
@@ -7,12 +7,12 @@ USER=iplant
 GROUP=iplant
 
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
-if [ -d "je-build" ]; then
-  rm -r je-build
+if [ -d "jex-build" ]; then
+  rm -r jex-build
 fi
-mkdir -p je-build/usr/local/bin
-mkdir -p je-build/var/log/jex-events
+mkdir -p jex-build/usr/local/bin
+mkdir -p jex-build/var/log/jex-events
 godep restore
 go build .
-cp jex-events je-build/usr/local/bin/
-fpm -s dir -t rpm --directories /var/log/jex-events --version $VERSION --iteration $ITERATION --epoch 0 --prefix / --name jex-events --verbose -C je-build --rpm-user $USER --rpm-group $GROUP -f .
+cp jex-events jex-build/usr/local/bin/
+fpm -s dir -t rpm --directories /var/log/jex-events --version $VERSION --iteration $ITERATION --epoch 0 --prefix / --name jex-events --verbose -C jex-build --rpm-user $USER --rpm-group $GROUP -f .
