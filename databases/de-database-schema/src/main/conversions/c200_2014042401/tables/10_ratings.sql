500f3aab0ec2856cc9294a92b8ffcd3d8cb4cd55
Paul Sarando <psarando@iplantcollaborative.org>
2014-11-06 13:12:34 -0700

    CORE-6100 CORE-5386 Added ratings conversion.

--- databases/de-database-schema/src/main/conversions/c200_2014042401/tables/10_ratings.sql
+++ databases/de-database-schema/src/main/conversions/c200_2014042401/tables/10_ratings.sql
@@ -10,3 +10,5 @@ ALTER TABLE ONLY ratings ADD COLUMN id UUID DEFAULT (uuid_generate_v1());
 ALTER TABLE ONLY ratings ADD COLUMN user_id UUID;
 ALTER TABLE ONLY ratings ADD COLUMN app_id UUID;
 
+-- Fix for CORE-6100 while we're at it
+UPDATE ratings SET rating = 5 WHERE rating > 5;
