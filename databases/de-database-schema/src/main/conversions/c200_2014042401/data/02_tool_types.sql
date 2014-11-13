13658a889a741f369ecb171fca7ac573387dd549
Dennis Roberts <dennis@iplantcollaborative.org>
2014-11-12 18:32:47 -0700

    CORE-6128: marked the "internal" tool type as hidden

--- databases/de-database-schema/src/main/conversions/c200_2014042401/data/02_tool_types.sql
+++ databases/de-database-schema/src/main/conversions/c200_2014042401/data/02_tool_types.sql
@@ -3,9 +3,9 @@ SET search_path = public, pg_catalog;
 --
 -- Add new tool types to the database.
 --
-INSERT INTO tool_types (id, name, label, description)
+INSERT INTO tool_types (id, name, label, description, hidden)
     VALUES ( '01E14110-1420-4DE0-8A70-B0DD420F6A84', 'internal', 'Internal DE tools.',
-             'Tools used internally by the Discovery Environment.');
+             'Tools used internally by the Discovery Environment.', true );
 
 --
 -- Add associations between tool types and compatible parameter types.
