96e18a177b4a93193a93e58838e3e6ba3a52dac9
Paul Sarando <psarando@iplantcollaborative.org>
2014-11-24 14:28:08 -0700

    CORE-6155 Updated tables/25_parameter_groups.sql
    Forgotten in commit 5d538c1282e95a003a4d90c275083e163a842903.

--- databases/de-database-schema/src/main/tables/25_parameter_groups.sql
+++ databases/de-database-schema/src/main/tables/25_parameter_groups.sql
@@ -6,9 +6,9 @@ SET search_path = public, pg_catalog;
 CREATE TABLE parameter_groups (
     id uuid NOT NULL DEFAULT uuid_generate_v1(),
     task_id uuid NOT NULL,
-    name character varying(255) NOT NULL,
+    name character varying(255),
     description text,
-    label character varying(255),
+    label character varying(255) NOT NULL,
     display_order int NOT NULL DEFAULT 0,
     is_visible boolean DEFAULT true
 );
