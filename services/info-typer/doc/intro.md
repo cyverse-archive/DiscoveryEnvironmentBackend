3390883c13cdeb4231564ea65a0144022156ea9c
Tony Edgin <tedgin@iplantcollaborative.org>
2014-12-18 14:35:47 -0700

    CORE-6312 scaffolding for info-typer AMQP service

---
+++ services/info-typer/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to info-typer
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ services/info-typer/test/info_typer/core_test.clj
@@ -0,0 +1,7 @@
+(ns info-typer.core-test
+  (:require [clojure.test :refer :all]
+            [info-typer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
