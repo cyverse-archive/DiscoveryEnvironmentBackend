23e1775ecc423b958bd1c91d766a57ba83a1ff40
Tony Edgin <tedgin@iplantcollaborative.org>
2014-12-22 15:45:49 -0700

    CORE-6312 fixed build.sh to match data-info

--- services/info-typer/build.sh
+++ services/info-typer/build.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
+set -x
+set -e
 
-for project in $(find . -name checkouts.sh -exec dirname '{}' \;); do
-  pushd $project
-  if [ -d ./checkouts ]; then
-    rm -r ./checkouts
-  fi
+ITERATION=$1
+USER=iplant
+GROUP=iplant
+BINNAME=info-typer
+BUILDDIR=$BINNAME-build
+BINDIR=/usr/local/lib/$BINNAME
+LOGDIR=/var/log/$BINNAME
+CONFDIR=/etc/info-typer
+REPODIR=conf/main
 
-  ./checkouts.sh
-
-  if [ -f ./install-checkouts.sh ]; then
-    ./install-checkouts.sh
-  fi
-  popd
-done
+VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
+if [ -d "$BUILDDIR" ]; then
+  rm -r $BUILDDIR
+fi
+mkdir -p $BUILDDIR/$BINDIR
+mkdir -p $BUILDDIR/$LOGDIR
+mkdir -p $BUILDDIR/$CONFDIR
+lein clean
+lein deps
+lein uberjar
+cp target/$BINNAME-standalone.jar $BUILDDIR/$BINDIR
+cp $REPODIR/* $BUILDDIR/$CONFDIR
+fpm -s dir -t rpm --directories $LOGDIR -d java-1.7.0-openjdk --version $VERSION --iteration $ITERATION --epoch 0 --prefix / --name $BINNAME --verbose -C $BUILDDIR --rpm-user $USER --rpm-group $GROUP -f .
