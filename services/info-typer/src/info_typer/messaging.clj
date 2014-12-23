e2cbd95defdfb568a84fd9f3cfbf0e084eebb671
Tony Edgin <tedgin@iplantcollaborative.org>
2014-12-23 16:03:33 -0700

    CORE-6312 combined messages and messaging

--- services/info-typer/src/info_typer/messaging.clj
+++ services/info-typer/src/info_typer/messaging.clj
@@ -1,15 +1,51 @@
 (ns info-typer.messaging
-  (:require [clojure.tools.logging :as log]
+  (:require [clojure.string :as string]
+            [clojure.tools.logging :as log]
+            [cheshire.core :as json]
+            [slingshot.slingshot :refer [throw+ try+]]
+            [clj-jargon.init :refer [with-jargon]]
+            [clj-jargon.item-info :as info]
+            [clj-jargon.metadata :as meta]
             [clojure-commons.error-codes :as ce]
             [info-typer.amqp :as amqp]
-            [info-typer.messages :as ftype])
+            [info-typer.config :as cfg]
+            [info-typer.irods :as irods])
   (:import [clojure.lang IPersistentMap]))
+
+
+(defn- filetype-message-handler
+  [irods-cfg payload]
+  (try+
+    (with-jargon irods-cfg [cm]
+      (let [parsed-map (json/parse-string payload true)
+            path       (:path parsed-map)]
+        (if (nil? path)
+          (throw+ {:error_code "ERR_INVALID_JSON" :payload payload}))
+        (if-not (info/exists? cm path)
+          (log/warn "[filetype-message-handler]" path
+            "does not exist, it probably got moved before the handler fired."))
+        (if (meta/attribute? cm path (cfg/garnish-type-attribute))
+          (log/warn "[filetype-message-handler]" path "already has an attribute called"
+            (cfg/garnish-type-attribute)))
+        (when (and (info/exists? cm path)
+                   (not (meta/attribute? cm path (cfg/garnish-type-attribute))))
+          (let [ctype (irods/content-type cm path)]
+            (when-not (or (nil? ctype) (string/blank? ctype))
+              (log/warn "[filetype-message-handler] adding type" ctype "to" path)
+              (meta/add-metadata cm path (cfg/garnish-type-attribute) ctype "")
+              (log/warn "[filetype-message-handler] done adding type" ctype "to" path))
+            (when (or (nil? ctype) (string/blank? ctype))
+              (log/warn "[filetype-message-handler] type was not detected for" path))))))
+    (catch ce/error? err
+      (log/error (ce/format-exception (:throwable &throw-context))))
+    (catch Exception e
+      (log/error (ce/format-exception e)))))
 
 
 (defn- dataobject-added
   "Event handler for 'data-object.added' events."
   [irods-cfg ^bytes payload]
-  (ftype/filetype-message-handler irods-cfg (String. payload "UTF-8")))
+  (filetype-message-handler irods-cfg (String. payload "UTF-8")))
 
 
 (defn- message-handler
