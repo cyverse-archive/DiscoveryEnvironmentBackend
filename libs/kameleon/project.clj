8d17490c3d6413c978043d3cbe8e385f84d9e57d
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-01-09 09:01:54 -0700

    Bumped the version to 4.1.1.

--- libs/kameleon/project.clj
+++ libs/kameleon/project.clj
@@ -1,4 +1,4 @@
-(defproject org.iplantc/kameleon "4.1.0"
+(defproject org.iplantc/kameleon "4.1.1"
   :description "Library for interacting with backend relational databases."
   :url "http://www.iplantcollaborative.org"
   :license {:name "BSD"
--- services/info-typer/project.clj
+++ services/info-typer/project.clj
@@ -9,7 +9,7 @@
     ""))
 
 
-(defproject info-typer "4.1.0"
+(defproject info-typer "4.1.1"
   :description "An AMQP based info type detection service for iRODS"
   :manifest {"Git-Ref" ~(git-ref)}
   :uberjar-name "info-typer-standalone.jar"
@@ -17,11 +17,11 @@
                  [org.clojure/tools.logging "0.3.1"]
                  [com.novemberain/langohr "2.11.0"]
                  [me.raynes/fs "1.4.6"]
-                 [org.iplantc/clj-jargon "4.1.0"]
-                 [org.iplantc/clojure-commons "4.1.0"]
-                 [org.iplantc/common-cli "4.1.0"]
-                 [org.iplantc/heuristomancer "4.1.0"]]
-  :plugins [[org.iplantc/lein-iplant-rpm "4.1.0"]]
+                 [org.iplantc/clj-jargon "4.1.1"]
+                 [org.iplantc/clojure-commons "4.1.1"]
+                 [org.iplantc/common-cli "4.1.1"]
+                 [org.iplantc/heuristomancer "4.1.1"]]
+  :plugins [[org.iplantc/lein-iplant-rpm "4.1.1"]]
   :main ^:skip-aot info-typer.core
   :profiles {:dev {:resource-paths ["conf/test"]}
              :uberjar {:aot :all}}
