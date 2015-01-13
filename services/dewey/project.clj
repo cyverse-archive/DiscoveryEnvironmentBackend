0f14cae0e16bbcd1214c09d7449f07cd51bc1399
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-01-13 15:05:17 -0700

    Bumped the backend version to 4.1.2

--- services/dewey/project.clj
+++ services/dewey/project.clj
@@ -7,7 +7,7 @@
       (string/trim (:out (sh "git" "rev-parse" "HEAD")))
       ""))
 
-(defproject org.iplantc/dewey "4.1.1"
+(defproject org.iplantc/dewey "4.1.2"
   :description "This is a RabbitMQ client responsible for keeping an elasticsearch index
                 synchronized with an iRODS repository using messages produced by iRODS."
   :url "http://www.iplantcollaborative.org"
@@ -28,14 +28,14 @@
                  [ring/ring-core "1.3.0"]
                  [ring/ring-jetty-adapter "1.3.0"]
                  [slingshot "0.10.3"]
-                 [org.iplantc/clj-jargon "4.1.1"]
-                 [org.iplantc/clojure-commons "4.1.1"]
-                 [org.iplantc/common-cli "4.1.1"]
+                 [org.iplantc/clj-jargon "4.1.2"]
+                 [org.iplantc/clojure-commons "4.1.2"]
+                 [org.iplantc/common-cli "4.1.2"]
                  [me.raynes/fs "1.4.6"]]
   :resource-paths []
   :profiles {:dev {:dependencies   [[midje "1.6.3"]]
                    :resource-paths ["dev-resource"]}}
-  :plugins [[org.iplantc/lein-iplant-rpm "4.1.1"]]
+  :plugins [[org.iplantc/lein-iplant-rpm "4.1.2"]]
   :iplant-rpm {:summary      "dewey"
                :dependencies ["iplant-service-config >= 0.1.0-5" "java-1.7.0-openjdk"]
                :config-files ["log4j.properties"]
