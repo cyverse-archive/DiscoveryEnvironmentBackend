989d14c074a1bcf101a10eea033bae387af3a12b
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-01-26 12:37:33 -0700

    Fixed the DOCKER_REPO setting...maybe.

--- services/docker-build.sh
+++ services/docker-build.sh
@@ -3,7 +3,6 @@ set -x
 set -e
 
 DOCKER_USER=discoenv
-DOCKER_REPO=
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
 
 docker run --rm -t -a stdout -a stderr -e "GIT_COMMIT=$(git rev-parse HEAD)" -v $(pwd):/build -v ~/.m2:/root/.m2 -w /build discoenv/buildenv lein uberjar
