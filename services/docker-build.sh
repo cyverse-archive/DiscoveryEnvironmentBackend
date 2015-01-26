6c4e5abed99fd8aeb434dc8254e4b0effe02f020
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-01-26 12:34:21 -0700

    Fixed the docker build command in docker-build.sh

--- services/docker-build.sh
+++ services/docker-build.sh
@@ -7,5 +7,5 @@ DOCKER_REPO=
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
 
 docker run --rm -t -a stdout -a stderr -e "GIT_COMMIT=$(git rev-parse HEAD)" -v $(pwd):/build -v ~/.m2:/root/.m2 -w /build discoenv/buildenv lein uberjar
-docker build --rm -t "$DOCKER_USER/$DOCKER_REPO:dev"
+docker build --rm -t "$DOCKER_USER/$DOCKER_REPO:dev" .
 docker push $DOCKER_USER/$DOCKER_REPO:dev
