aae0ecfa544e1a43f6be204a67dad8c403ba84d0
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-01-26 13:29:57 -0700

    Added a command to the docker-build.sh script for kifshare.

--- services/kifshare/docker-build.sh
+++ services/kifshare/docker-build.sh
@@ -5,6 +5,6 @@ set -e
 DOCKER_USER=discoenv
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
 
-docker run --rm -t -a stdout -a stderr -e "GIT_COMMIT=$(git rev-parse HEAD)" -v $(pwd):/build -v ~/.m2:/root/.m2 -w /build discoenv/buildenv grunt build-resources && lein uberjar
+docker run --rm -t -a stdout -a stderr -e "GIT_COMMIT=$(git rev-parse HEAD)" -v $(pwd):/build -v ~/.m2:/root/.m2 -w /build discoenv/buildenv npm install && grunt build-resources && lein uberjar
 docker build --rm -t "$DOCKER_USER/$DOCKER_REPO:dev" .
 docker push $DOCKER_USER/$DOCKER_REPO:dev
