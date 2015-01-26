ae1c35e3d1f9cbe5e90c322fde2f9dc403283325
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-01-26 13:54:32 -0700

    Fixed the docker build for jex-events.

--- services/jex-events/docker-build.sh
+++ services/jex-events/docker-build.sh
@@ -5,6 +5,6 @@ set -e
 DOCKER_USER=discoenv
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
 
-docker run --rm -t -a stdout -a stderr -e "GIT_COMMIT=$(git rev-parse HEAD)" -v $(pwd):/build -w /build discoenv/buildenv go build
+docker run --rm -t -a stdout -a stderr -e "GIT_COMMIT=$(git rev-parse HEAD)" -v $(pwd):/build -w /build discoenv/buildenv ./intra-container-build.sh
 docker build --rm -t "$DOCKER_USER/$DOCKER_REPO:dev" .
 docker push $DOCKER_USER/$DOCKER_REPO:dev
