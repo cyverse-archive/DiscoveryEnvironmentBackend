b2ac53c8963c87806063f8f29aabc66434fc7d1d
Paul Sarando <psarando@iplantcollaborative.org>
2015-02-04 15:28:10 -0700

    CORE-6419 Added app_documentation table foreign key.

--- databases/de-database-schema/src/main/constraints/10_app_documentation.sql
+++ databases/de-database-schema/src/main/constraints/10_app_documentation.sql
@@ -6,3 +6,11 @@ SET search_path = public, pg_catalog;
 ALTER TABLE app_documentation
 ADD CONSTRAINT app_documentation_pkey
 PRIMARY KEY (app_id);
+
+--
+-- app_documentation table foreign key.
+--
+ALTER TABLE ONLY app_documentation
+ADD CONSTRAINT app_documentation_app_id_fkey
+FOREIGN KEY (app_id)
+REFERENCES apps(id) ON DELETE CASCADE;
