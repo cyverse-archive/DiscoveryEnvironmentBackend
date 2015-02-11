2ba72104c616412d40315b24d478153a13215282
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-02-11 12:51:46 -0700

    The name and tag columns must be a unique combo in the container_images table.

--- databases/de-database-schema/src/main/tables/70_container_images.sql
+++ databases/de-database-schema/src/main/tables/70_container_images.sql
@@ -7,5 +7,6 @@ CREATE TABLE container_images (
   id uuid UNIQUE  NOT NULL DEFAULT uuid_generate_v1(),   -- primary key
   name text NOT NULL, -- name used to indicate which image to pull down. Could be a UUID, but don't do that.
   tag text NOT NULL,  -- tag used to pull down an image. We'll default it to 'latest'
-  url text            -- URL containing more information about the image (ex: docker hub URL)
+  url text,           -- URL containing more information about the image (ex: docker hub URL)
+  unique (name, tag)
 );
