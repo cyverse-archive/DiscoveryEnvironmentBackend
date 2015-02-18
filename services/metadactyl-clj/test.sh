2238957f11be1f61520281adccde63513b000904
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-02-18 08:49:24 -0700

    Modified the test.sh script for metadactyl to kill the de-db container first.

--- services/metadactyl-clj/test.sh
+++ services/metadactyl-clj/test.sh
@@ -3,7 +3,15 @@
 set -e
 set -x
 
+if [ $(docker ps | grep de-db | wc -l) -gt 0 ]; then 
+    docker kill de-db
+fi
+
+if [ $(docker ps -a | grep de-db | wc -l) -gt 0 ]; then
+    docker rm de-db
+fi
+
 docker run --name de-db -e POSTGRES_PASSWORD=notprod -d discoenv/de-db
 sleep 2
 docker run --rm --link de-db:postgres discoenv/de-db-loader
-docker run -a stdout -a stderr -i -t --rm -v $(pwd):/build -v ~/.m2:/root/.m2 -w /build --link de-db:postgres clojure lein test
+docker run -i -t --rm -v $(pwd):/build -v ~/.m2:/root/.m2 -w /build --link de-db:postgres clojure lein test
