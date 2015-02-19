1fa15f5cecf400a477d29753e969c1147ee15edc
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-02-19 11:27:42 -0700

    Added more tooling scripts for metadactyl

--- services/metadactyl-clj/test.sh
+++ services/metadactyl-clj/test.sh
@@ -3,7 +3,15 @@
 set -e
 set -x
 
-$(boot2docker shellinit)
+CMD=$1
+
+if [ -z $CMD ]; then
+    CMD=test
+fi
+
+if [[ "$(uname)" == 'Darwin' ]]; then
+    $(boot2docker shellinit)
+fi
 
 if [ $(docker ps | grep de-db | wc -l) -gt 0 ]; then 
     docker kill de-db
@@ -13,7 +21,7 @@ if [ $(docker ps -a | grep de-db | wc -l) -gt 0 ]; then
     docker rm de-db
 fi
 
-docker run --name de-db -e POSTGRES_PASSWORD=notprod -d discoenv/de-db
+docker run --name de-db -e POSTGRES_PASSWORD=notprod -d -p 5432:5432 discoenv/de-db
 sleep 2
 docker run --rm --link de-db:postgres discoenv/de-db-loader
-docker run -i -t --rm -v $(pwd):/build -v ~/.m2:/root/.m2 -w /build --link de-db:postgres clojure lein test
+docker run -i -t --rm -v $(pwd):/build -v ~/.m2:/root/.m2 -w /build --link de-db:postgres clojure lein $CMD
