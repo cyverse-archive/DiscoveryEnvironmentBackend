3983231c851b6f1e0e9e35df40eb1a202dae4dc3
Tony Edgin <tedgin@iplantcollaborative.org>
2015-03-02 16:15:51 -0700

    CORE-6551 updated lein-iplant-rpm dependencies
    This was causing confusion in the info-typer build

--- lein-plugins/lein-iplant-rpm/project.clj
+++ lein-plugins/lein-iplant-rpm/project.clj
@@ -6,8 +6,8 @@
             :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
   :classifiers [["javadoc" :javadoc]
                 ["sources" :sources]]
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [fleet "0.9.5"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [fleet "0.10.2"]]
   :repositories {"iplantCollaborative"
                  "http://katic.iplantcollaborative.org/archiva/repository/internal/"}
   :deploy-repositories [["sonatype-nexus-staging"
