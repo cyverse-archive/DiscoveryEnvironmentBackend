3eda48f5a2294d02024d0042dae56ab853773ab2
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-03-13 08:51:21 -0700

    Fixed the version for clj-jargon.

--- libs/clj-jargon/project.clj
+++ libs/clj-jargon/project.clj
@@ -10,14 +10,14 @@
                  [org.iplantc/clojure-commons "4.2.1"]
                  [org.slf4j/slf4j-api "1.7.10"]
                  [org.slf4j/slf4j-log4j12 "1.7.10"]
-                 [org.iplantc.core.jargon/jargon-core "4.2.1"
+                 [org.iplantc.core.jargon/jargon-core "4.0.2.1.1"
                   :exclusions [[org.jglobus/JGlobus-Core]
                                [org.slf4j/slf4j-api]
                                [org.slf4j/slf4j-log4j12]]]
-                 [org.iplantc.core.jargon/jargon-data-utils "4.2.1"
+                 [org.iplantc.core.jargon/jargon-data-utils "4.0.2.1.1"
                   :exclusions [[org.slf4j/slf4j-api]
                                [org.slf4j/slf4j-log4j12]]]
-                 [org.iplantc.core.jargon/jargon-ticket "4.2.1"
+                 [org.iplantc.core.jargon/jargon-ticket "4.0.2.1.1"
                   :exclusions [[org.slf4j/slf4j-api]
                                [org.slf4j/slf4j-log4j12]]]
                  [slingshot "0.12.2"]]
