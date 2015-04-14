843158e57c43df363d5b461f69de2a211ebc980e
Dennis Roberts <dennis@iplantcollaborative.org>
2015-04-14 10:49:02 -0700

    moved some constraint additions to a new file

--- databases/de-database-schema/src/main/tables/99_constraints.sql
+++ databases/de-database-schema/src/main/tables/99_constraints.sql
@@ -826,22 +826,6 @@ ALTER TABLE ONLY logins
     REFERENCES users(id);
 
 --
--- Foreign key constraint for the template_id field of the metadata_template_attrs table.
---
-ALTER TABLE ONLY metadata_template_attrs
-    ADD CONSTRAINT metadata_template_attrs_template_id_fkey
-    FOREIGN KEY (template_id)
-    REFERENCES metadata_templates(id);
-
---
--- Foreign key constraint for the attribute_id field of the metadata_template_attrs table.
---
-ALTER TABLE ONLY metadata_template_attrs
-    ADD CONSTRAINT metadata_template_attrs_attribute_id_fkey
-    FOREIGN KEY (attribute_id)
-    REFERENCES metadata_attributes(id);
-
---
 -- The primary key for the user_preferences table.
 --
 ALTER TABLE ONLY user_preferences
