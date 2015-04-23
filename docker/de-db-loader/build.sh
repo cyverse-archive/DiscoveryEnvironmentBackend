b2cf212fd6893fc99521740a66b1e83f0fce9c9e
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-04-23 13:35:43 -0700

    Fixed the de-db-loader build.

--- docker/de-db-loader/build.sh
+++ docker/de-db-loader/build.sh
@@ -10,4 +10,4 @@ pushd ../../tools/facepalm/
 docker run --rm -t -a stdout -a stderr -e "GIT_COMMIT=$(git rev-parse HEAD)" -v $(pwd):/build -v ~/.m2:/root/.m2 -w /build discoenv/buildenv lein uberjar
 popd
 cp ../../tools/facepalm/target/facepalm-standalone.jar .
-docker build -t discoenv/de-db-loader .
+docker build -t $DOCKER_USER/de-db-loader:dev .
