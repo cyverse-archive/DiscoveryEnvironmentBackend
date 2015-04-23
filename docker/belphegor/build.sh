9c63bddb2c170a15d281460d6fdc1d567efef869
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-04-23 14:57:21 -0700

    Fixed belphegor's build.sh file to use DOCKER_USER.

--- docker/belphegor/build.sh
+++ docker/belphegor/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 curl -O https://everdene.iplantcollaborative.org/jenkins/job/DiscoveryEnvironment_UI_Beta/lastSuccessfulBuild/artifact/applications/belphegor/build/libs/wars/belphegor.war
-docker build -t discoenv/belphegor:dev .
-docker push discoenv/belphegor:dev
+docker build -t $DOCKER_USER/belphegor:dev .
+docker push $DOCKER_USER/belphegor:dev
