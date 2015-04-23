7f7d8b354043d513dd39f6ad7855cb2fcfa86730
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-04-23 12:49:05 -0700

    DOCKER_USER is pulled from the environment, now.

--- tools/facepalm/docker-build.sh
+++ tools/facepalm/docker-build.sh
@@ -3,5 +3,5 @@ set -x
 set -e
 
 docker run --rm -t -a stdout -a stderr -e "GIT_COMMIT=$(git rev-parse HEAD)" -v $(pwd):/build -v ~/.m2:/root/.m2 -w /build discoenv/buildenv lein uberjar
-docker build --rm -t discoenv/facepalm:dev .
-docker push discoenv/facepalm:dev
+docker build --rm -t $DOCKER_USER/facepalm:dev .
+docker push $DOCKER_USER/facepalm:dev
