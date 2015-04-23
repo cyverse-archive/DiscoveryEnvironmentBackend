0b508fe910c54b79af28ff65264e88d07bc72d5b
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-04-23 13:54:50 -0700

    Fixed the kifshare build script to use the correct DOCKER_USER setting.

--- services/kifshare/docker-build.sh
+++ services/kifshare/docker-build.sh
@@ -2,7 +2,6 @@
 set -x
 set -e
 
-DOCKER_USER=discoenv
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
 
 docker run --rm -t -a stdout -a stderr -e "GIT_COMMIT=$(git rev-parse HEAD)" -v $(pwd):/build -v ~/.m2:/root/.m2 -w /build discoenv/buildenv ./intra-container-build.sh
