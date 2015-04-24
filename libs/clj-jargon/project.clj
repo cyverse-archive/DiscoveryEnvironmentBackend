1bb5f6974b8b51ce82d317c54eb8e519a39b13bd
Dennis Roberts <dennis@iplantcollaborative.org>
2015-04-24 16:18:21 -0700

    restored the correct Jargon versions

--- libs/clj-jargon/project.clj
+++ libs/clj-jargon/project.clj
@@ -8,14 +8,14 @@
                  [org.iplantc/clojure-commons "5.0.0"]
                  [org.slf4j/slf4j-api "1.7.12"]
                  [org.slf4j/slf4j-log4j12 "1.7.12"]
-                 [org.iplantc.core.jargon/jargon-core "5.0.0"
+                 [org.iplantc.core.jargon/jargon-core "4.0.2.1.3"
                   :exclusions [[org.jglobus/JGlobus-Core]
                                [org.slf4j/slf4j-api]
                                [org.slf4j/slf4j-log4j12]]]
-                 [org.iplantc.core.jargon/jargon-data-utils "5.0.0"
+                 [org.iplantc.core.jargon/jargon-data-utils "4.0.2.1.1"
                   :exclusions [[org.slf4j/slf4j-api]
                                [org.slf4j/slf4j-log4j12]]]
-                 [org.iplantc.core.jargon/jargon-ticket "5.0.0"
+                 [org.iplantc.core.jargon/jargon-ticket "4.0.2.1.1"
                   :exclusions [[org.slf4j/slf4j-api]
                                [org.slf4j/slf4j-log4j12]]]
                  [slingshot "0.12.2"]]
