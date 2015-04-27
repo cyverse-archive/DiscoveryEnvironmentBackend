b3364651ba8748b036fb0f4602d0e067c3f00245
Dennis Roberts <dennis@iplantcollaborative.org>
2015-04-27 16:22:04 -0700

    CORE-6724: removed some debugging code :$

--- services/metadactyl-clj/src/metadactyl/routes/callbacks.clj
+++ services/metadactyl-clj/src/metadactyl/routes/callbacks.clj
@@ -13,8 +13,7 @@
          :summary "Update the status of of a DE analysis."
          :notes "The jex-events service calls this endpoint when the status of a DE analysis
          changes"
-         #_(service/trap uri callbacks/update-de-job-status body)
-         (service/trap uri (constantly {})))
+         (service/trap uri callbacks/update-de-job-status body))
 
   (POST* "/agave-job/:job-id" [:as {:keys [uri]}]
          :path-params [job-id :- AnalysisIdPathParam]
