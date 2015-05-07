d9c8aa77154830a91cee670c464d1b7429915bbd
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-05-07 12:22:42 -0700

    user-sessions now logs to stdout with log4j.

--- services/user-sessions/project.clj
+++ services/user-sessions/project.clj
@@ -16,6 +16,7 @@
   :aot [user-sessions.core]
   :main user-sessions.core
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.logging "0.3.0"]
                  [org.iplantc/common-cli "5.0.0"]
                  [org.iplantc/common-cfg "5.0.0"]
                  [org.iplantc/kameleon "5.0.0"]
