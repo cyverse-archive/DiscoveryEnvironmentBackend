e59908373fda314e763c8115a2460f7ebb507917
Paul Sarando <psarando@iplantcollaborative.org>
2015-05-11 11:34:42 -0700

    CORE-6751 Reformatted some Swagger docs.

--- services/data-info/src/data_info/routes/data.clj
+++ services/data-info/src/data_info/routes/data.clj
@@ -14,13 +14,14 @@
       :query [params SecuredQueryParamsRequired]
       :body [body (describe Paths "The paths to create.")]
       :return Paths
-      :summary "Create Directories."
-      :description "Creates a directory, as well as any intermediate directories that do not already
-      exist, given as a path in the request. For example, if the path
-      `/tempZone/home/rods/test1/test2/test3` is given in the request, the
-      `/tempZone/home/rods/test1` directory does not exist, and the requesting user has write
-      permissions on the `/tempZone/home/rods` folder, then all 3 `test*` folders will be created
-      for the requesting user.
+      :summary "Create Directories"
+      :description
+"Creates a directory, as well as any intermediate directories that do not already exist, given as a
+path in the request. For example, if the path `/tempZone/home/rods/test1/test2/test3` is given in
+the request, the `/tempZone/home/rods/test1` directory does not exist, and the requesting user has
+write permissions on the `/tempZone/home/rods` folder, then all 3 `test*` folders will be created
+for the requesting user.
+
 #### Error Codes:
 
       ERR_BAD_OR_MISSING_FIELD, ERR_NOT_WRITEABLE, ERR_EXISTS, ERR_DOES_NOT_EXIST,
@@ -33,10 +34,12 @@
       :query [params SecuredQueryParamsRequired]
       :body [body (describe MetadataSaveRequest "The metadata save request.")]
       :return FileStat
-      :summary "Exporting Metadata to a File."
-      :description "Exports file/folder details in a JSON format (similar to the /stat-gatherer
-      endpoint response), including all Metadata Template AVUs and IRODS AVUs visible to the
-      requesting user, to the file specified in the request.
+      :summary "Exporting Metadata to a File"
+      :description
+"Exports file/folder details in a JSON format (similar to the /stat-gatherer endpoint response),
+including all Metadata Template AVUs and IRODS AVUs visible to the requesting user, to the file
+specified in the request.
+
 #### Error codes:
 
       ERR_INVALID_JSON, ERR_EXISTS, ERR_DOES_NOT_EXIST, ERR_NOT_READABLE,
