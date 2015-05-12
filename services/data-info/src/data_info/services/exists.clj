95ac3871440a725dd1a7d5b6a3cb0ad7f3378f3e
Paul Sarando <psarando@iplantcollaborative.org>
2015-05-11 18:05:43 -0700

    Refactor data-info.services.exists
    Refactored `with-jargon` and `user-exists` check outside of loop that calls `path-exists-for-user?` and into `do-exists`.
    Used `juxt` instead of `hash-map`.
    No longer need to URL decode the POST body of the request, which can actually prevent a path like "/testZone/home/test/foo%20bar%20baz" from being found, which is a valid path.

--- services/data-info/src/data_info/services/exists.clj
+++ services/data-info/src/data_info/services/exists.clj
@@ -10,32 +10,17 @@
             [data-info.util.logging :as log]
             [data-info.util.validators :as duv]))
 
-
-(defn- url-encoded?
-  [string-to-check]
-  (re-seq #"\%[A-Fa-f0-9]{2}" string-to-check))
-
-
-(defn- url-decode
-  [string-to-decode]
-  (if (url-encoded? string-to-decode)
-    (url/url-decode string-to-decode)
-    string-to-decode))
-
-
 (defn- path-exists-for-user?
-  [user path]
-  (let [path (ft/rm-last-slash (url-decode path))]
-    (with-jargon (cfg/jargon-cfg) [cm]
-      (duv/user-exists cm user)
-      (and (item/exists? cm path)
-           (perm/is-readable? cm user path)))))
-
+  [cm user path]
+  (let [path (ft/rm-last-slash path)]
+    (and (item/exists? cm path)
+         (perm/is-readable? cm user path))))
 
 (defn do-exists
   [{user :user} {paths :paths}]
-  {:paths (into {}
-                (map #(hash-map % (path-exists-for-user? user %)) (set paths)))})
+  (with-jargon (cfg/jargon-cfg) [cm]
+    (duv/user-exists cm user)
+    {:paths (into {} (map (juxt identity (partial path-exists-for-user? cm user)) (set paths)))}))
 
 (with-pre-hook! #'do-exists
   (fn [params body]
