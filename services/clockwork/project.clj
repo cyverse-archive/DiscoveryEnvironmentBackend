920086f84b8dc7374f2c78b9b5ed6b2338c63cac
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-05-12 14:30:04 -0700

    clockwork should be logging correctly now.

--- services/clockwork/project.clj
+++ services/clockwork/project.clj
@@ -16,32 +16,25 @@
   :uberjar-name "clockwork-standalone.jar"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/java.jdbc "0.2.3"]
-                 [org.clojure/tools.logging "0.2.3"]
                  [cheshire "5.0.2"]
                  [clj-http "0.6.5"]
                  [clj-time "0.4.5"]
                  [clojurewerkz/quartzite "1.0.1"]
                  [com.cemerick/url "0.0.7"]
                  [korma "0.3.0-RC5"]
-                 [log4j "1.2.17"]
-                 [org.iplantc/clj-jargon "5.0.0"]
+                 [org.iplantc/clj-jargon "5.0.0"
+                   :exclusions [[org.slf4j/slf4j-api]
+                                [org.slf4j/slf4j-log4j12]
+                                [log4j]]]
                  [org.iplantc/clojure-commons "5.0.0"]
                  [org.iplantc/common-cli "5.0.0"]
                  [org.iplantc/kameleon "5.0.0"]
                  [me.raynes/fs "1.4.6"]
-                 [org.slf4j/slf4j-api "1.7.2"]
-                 [org.slf4j/slf4j-log4j12 "1.6.6"]
                  [slingshot "0.10.3"]]
   :plugins [[org.iplantc/lein-iplant-rpm "5.0.0"]]
   :profiles {:dev     {:resource-paths ["resources/test"]}
              :uberjar {:aot :all}}
   :main ^:skip-aot clockwork.core
-  :iplant-rpm {:summary "Scheduled jobs for the iPlant Discovery Environment"
-               :provides "clockwork"
-               :dependencies ["iplant-service-config >= 0.1.0-5"
-                              "java-1.7.0-openjdk"]
-               :config-files ["log4j.properties"]
-               :config-path "resources/main"}
   :uberjar-exclusions [#"BCKEY.SF"]
   :repositories [["sonatype-nexus-snapshots"
                   {:url "https://oss.sonatype.org/content/repositories/snapshots"}]]
