c7cb1164795a0637c782c7ed805c54cacddc1f50
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-05-12 15:40:08 -0700

    infosquito now logs JSON with log4j2.

--- services/Infosquito/project.clj
+++ services/Infosquito/project.clj
@@ -19,8 +19,12 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [postgresql "9.1-901-1.jdbc4"]
                  [org.clojure/java.jdbc "0.3.5"]
-                 [org.clojure/tools.logging "0.3.0"]
-                 [cheshire "5.3.1"]
+                 [cheshire "5.3.1"
+                  :exclusions [[com.fasterxml.jackson.dataformat/jackson-dataformat-cbor]
+                               [com.fasterxml.jackson.dataformat/jackson-dataformat-smile]
+                               [com.fasterxml.jackson.core/jackson-annotations]
+                               [com.fasterxml.jackson.core/jackson-databind]
+                               [com.fasterxml.jackson.core/jackson-core]]]
                  [clojurewerkz/elastisch "2.0.0"]
                  [com.novemberain/langohr "2.11.0"]
                  [slingshot "0.10.3"]
@@ -29,10 +33,6 @@
                  [org.iplantc/common-cli "5.0.0"]]
   :profiles {:dev {:resource-paths ["dev-resources"]}}
   :plugins [[org.iplantc/lein-iplant-rpm "5.0.0"]]
-  :iplant-rpm {:summary      "infosquito"
-               :dependencies ["iplant-service-config >= 0.1.0-5" "java-1.7.0-openjdk"]
-               :config-files ["log4j.properties"]
-               :config-path  "conf"}
   :repositories [["sonatype-nexus-snapshots"
                   {:url "https://oss.sonatype.org/content/repositories/snapshots"}]]
   :deploy-repositories [["sonatype-nexus-staging"
