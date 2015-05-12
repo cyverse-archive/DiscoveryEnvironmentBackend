c47b709ddb38caaff30611f1e119dd8c577f7f15
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-05-12 16:16:22 -0700

    tree-urls now logs JSON with log4j2.

--- services/tree-urls/project.clj
+++ services/tree-urls/project.clj
@@ -21,14 +21,16 @@
                  [org.iplantc/kameleon "5.0.0"]
                  [io.aviso/pretty "0.1.17"]
                  [me.raynes/fs "1.4.6"]
-                 [cheshire "5.3.1"]
+                 [cheshire "5.3.1"
+                   :exclusions [[com.fasterxml.jackson.dataformat/jackson-dataformat-cbor]
+                                [com.fasterxml.jackson.dataformat/jackson-dataformat-smile]
+                                [com.fasterxml.jackson.core/jackson-annotations]
+                                [com.fasterxml.jackson.core/jackson-databind]
+                                [com.fasterxml.jackson.core/jackson-core]]]
                  [compojure "1.1.6"]
                  [midje "1.6.3"]
                  [ring "1.2.1"]
                  [ring/ring-json "0.3.1"]]
-  :iplant-rpm {:summary "DE API for managing tree urls."
-               :provides "tree-urls"
-               :dependencies ["iplant-service-config >= 0.1.0-5" "java-1.7.0-openjdk"]}
   :plugins [[lein-ring "0.8.10"]
             [lein-midje "3.1.1"]
             [org.iplantc/lein-iplant-rpm "5.0.0"]])
