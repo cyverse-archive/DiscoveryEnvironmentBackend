e4d1f7f5d2ce12db8a6b96afb399c09d29164232
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-05-18 12:18:25 -0700

    Added --rm to the remaining docker build commands that didn't have it.

--- docker/belphegor/build.sh
+++ docker/belphegor/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 curl -O https://everdene.iplantcollaborative.org/jenkins/job/DiscoveryEnvironment_UI_Beta/lastSuccessfulBuild/artifact/applications/belphegor/build/libs/wars/belphegor.war
-docker build -t $DOCKER_USER/belphegor:dev .
+docker build --rm -t $DOCKER_USER/belphegor:dev .
 docker push $DOCKER_USER/belphegor:dev
--- docker/de/build.sh
+++ docker/de/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 curl -O https://everdene.iplantcollaborative.org/jenkins/job/DiscoveryEnvironment_UI_Dev/lastSuccessfulBuild/artifact/applications/de-webapp/build/libs/wars/de.war
-docker build -t discoenv/de:dev .
+docker build --rm -t discoenv/de:dev .
 docker push discoenv/de:dev
