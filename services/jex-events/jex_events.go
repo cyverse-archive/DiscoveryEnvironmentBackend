27f71fc4881d3c2abc609819e15f938702325d4d
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-05-26 09:50:17 -0700

    Modified jex-events to log in JSON that can be used by Kibana.

--- services/jex-events/jex_events.go
+++ services/jex-events/jex_events.go
@@ -32,16 +32,38 @@ func (l LoggerFunc) Write(logbuf []byte) (n int, err error) {
 	return l(logbuf)
 }
 
+// LogMessage represents a message that will be logged in JSON format.
+type LogMessage struct {
+	Service  string `json:"service"`
+	Artifact string `json:"art-id"`
+	Group    string `json:"group-id"`
+	Level    string `json:"level"`
+	Time     int64  `json:"timeMillis"`
+	Message  string `json:"message"`
+}
+
+// NewLogMessage returns a pointer to a new instance of LogMessage.
+func NewLogMessage(message string) *LogMessage {
+	lm := &LogMessage{
+		Service:  "jex-events",
+		Artifact: "jex-events",
+		Group:    "org.iplantc",
+		Level:    "INFO",
+		Time:     time.Now().UnixNano() / int64(time.Millisecond),
+		Message:  message,
+	}
+	return lm
+}
+
 // LogWriter writes to stdout with a custom timestamp.
 func LogWriter(logbuf []byte) (n int, err error) {
-	const layout = "2006-01-02@15:04:05.000"
-	t := time.Now().Format(layout)
-	return os.Stdout.Write(
-		append(
-			[]byte(fmt.Sprintf("%s de.jex-events.general ", t)),
-			logbuf...,
-		),
-	)
+	m := NewLogMessage(string(logbuf[:]))
+	j, err := json.Marshal(m)
+	if err != nil {
+		return 0, err
+	}
+	j = append(j, []byte("\n")...)
+	return os.Stdout.Write(j)
 }
 
 func init() {
