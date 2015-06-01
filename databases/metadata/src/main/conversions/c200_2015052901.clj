ebe2701449359a46d837451d7e61bdc0df2fb140
Dennis Roberts <dennis@iplantcollaborative.org>
2015-06-01 11:39:39 -0700

    CORE-6800: fixed a foreign key constraint violation that was occurring when running the metadata database conversion for 2.0.0:20150529.01

--- databases/metadata/src/main/conversions/c200_2015052901.clj
+++ databases/metadata/src/main/conversions/c200_2015052901.clj
@@ -6,6 +6,18 @@
   "The destination database version."
   "2.0.0:20150529.01")
 
+(defn- metadata-complete-avu-subselect
+  []
+  (subselect :avus
+             (fields :id)
+             (where {:attribute "Metadata complete"})))
+
+(defn- delete-metadata-complete-template-instance-records
+  []
+  (println "\t* deleting the template instance records fro 'Metadata complete' AVUs")
+  (delete :template_instances
+          (where {:avu_id [in (metadata-complete-avu-subselect)]})))
+
 (defn- delete-metadata-complete-template-attr
   []
   (println "\t* deleting 'Metadata complete' AVUs")
@@ -15,4 +27,5 @@
   "Performs the conversion for this version of the database"
   []
   (println "Performing the conversion for" version)
+  (delete-metadata-complete-template-instance-records)
   (delete-metadata-complete-template-attr))
