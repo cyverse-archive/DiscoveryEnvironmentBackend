4384091a31d04e19394b9a1fb51a5385071ae8e4
Jonathan Strootman <jstroot@iplantcollaborative.org>
2015-06-02 09:29:00 -0700

    Removed deprecated UI docker builds
    These builds are performed through the UI repo.

--- docker/belphegor/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-curl -O https://everdene.iplantcollaborative.org/jenkins/job/DiscoveryEnvironment_UI_Beta/lastSuccessfulBuild/artifact/applications/belphegor/build/libs/wars/belphegor.war
-docker build --rm -t $DOCKER_USER/belphegor:dev .
-docker push $DOCKER_USER/belphegor:dev
