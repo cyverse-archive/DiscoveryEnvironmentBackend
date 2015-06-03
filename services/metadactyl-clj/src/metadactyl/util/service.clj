e0b2be7fa97cd421227547f2712d2bd5a7cb8210
Dennis Roberts <dennis@iplantcollaborative.org>
2015-06-03 13:05:34 -0700

    CORE-6800: modified metadactyl.util.service to use the functions in clojure-commons.assertions

--- services/metadactyl-clj/src/metadactyl/util/service.clj
+++ services/metadactyl-clj/src/metadactyl/util/service.clj
@@ -5,6 +5,7 @@
   (:require [cheshire.core :as cheshire]
             [clojure.string :as string]
             [clojure.tools.logging :as log]
+            [clojure-commons.assertions :as ca]
             [clojure-commons.error-codes :as ce]
             [metadactyl.util.coercions :as mc]))
 
@@ -68,52 +69,11 @@
   [action schema func & args]
   (ce/trap action #(success-response (mc/coerce! schema (apply func args)))))
 
-(defn not-found
-  "Throws an exception indicating that an object wasn't found."
-  [desc id]
-  (throw+ {:error_code ce/ERR_NOT_FOUND
-           :reason     (string/join " " [desc id "not found"])}))
-
-(defn not-owner
-  "Throws an exception indicating that the user isn't permitted to perform the requested option."
-  [desc id]
-  (throw+ {:error_code ce/ERR_NOT_OWNER
-           :reason     (str "authenticated user doesn't own " desc ", " id)}))
-
-(defn not-unique
-  "Throws an exception indicating that multiple objects were found when only one was expected."
-  [desc id]
-  (throw+ {:error_code ce/ERR_NOT_UNIQUE
-           :reason     (string/join " " [desc id "not unique"])}))
-
-(defn bad-request
-  "Throws an exception indicating that the incoming request is invalid."
-  [reason]
-  (throw+ {:error_code ce/ERR_BAD_REQUEST
-           :reason     reason}))
-
-(defn assert-found
-  "Asserts that an object to modify or retrieve was found."
-  [obj desc id]
-  (if (nil? obj)
-    (not-found desc id)
-    obj))
-
-(defn assert-not-found
-  "Asserts that an object that is being created is not a duplicate."
-  [obj desc id]
-  (when-not (nil? obj)
-    (not-unique desc id)))
-
-(defn assert-valid
-  "Throws an exception if an arbitrary expression is false."
-  [valid? & msgs]
-  (when-not valid?
-    (throw+ {:error_code ce/ERR_ILLEGAL_ARGUMENT
-             :message    (string/join " " msgs)})))
-
-(defn request-failure
-  "Throws an exception indicating that a request failed for an unexpected reason."
-  [& msgs]
-  (throw+ {:error_code ce/ERR_REQUEST_FAILED
-           :message    (string/join " " msgs)}))
+(def not-found ca/not-found)
+(def not-owner ca/not-owner)
+(def not-unique ca/not-unique)
+(def bad-request ca/bad-request)
+(def assert-found ca/assert-found)
+(def assert-not-found ca/assert-not-found)
+(def assert-valid ca/assert-valid)
+(def request-failure ca/request-failure)
