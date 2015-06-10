3accd6c02e0de5565745382f9b73721ae8b4e919
Dennis Roberts <dennis@iplantcollaborative.org>
2015-06-10 09:04:36 -0700

    CORE-6800: added the most recent versions to the initialization script for the version table

--- databases/metadata/src/main/data/99_version.sql
+++ databases/metadata/src/main/data/99_version.sql
@@ -3,3 +3,5 @@ SET search_path = public, pg_catalog;
 INSERT INTO version (version) VALUES ('1.8.9:20140625.01');
 INSERT INTO version (version) VALUES ('1.8.9:20140707.01');
 INSERT INTO version (version) VALUES ('1.9.2:20140902.01');
+INSERT INTO version (version) VALUES ('2.0.0:20150529.01');
+INSERT INTO version (version) VALUES ('2.0.0:20150601.01');
