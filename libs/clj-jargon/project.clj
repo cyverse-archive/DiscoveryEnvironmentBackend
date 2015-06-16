af4858f46319d667bd99e2b3a17b587b7e4b82d9
Tony Edgin <tedgin@iplantcollaborative.org>
2015-06-16 15:37:15 -0700

    updated to Jargon 4.0.2.3-RELEASE

--- libs/clj-jargon/project.clj
+++ libs/clj-jargon/project.clj
@@ -5,20 +5,20 @@
             :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.iplantc/clojure-commons "5.0.0"]
-                 [org.slf4j/slf4j-api "1.7.12"]
-                 [org.slf4j/slf4j-log4j12 "1.7.12"]
-                 [org.iplantc.core.jargon/jargon-core "4.0.2.1.3"
+                 [org.irods.jargon/jargon-core "4.0.2.3-RELEASE"
                   :exclusions [[org.jglobus/JGlobus-Core]
                                [org.slf4j/slf4j-api]
                                [org.slf4j/slf4j-log4j12]]]
-                 [org.iplantc.core.jargon/jargon-data-utils "4.0.2.1.1"
+                 [org.irods.jargon/jargon-data-utils "4.0.2.3-RELEASE"
                   :exclusions [[org.slf4j/slf4j-api]
                                [org.slf4j/slf4j-log4j12]]]
-                 [org.iplantc.core.jargon/jargon-ticket "4.0.2.1.1"
+                 [org.irods.jargon/jargon-ticket "4.0.2.3-RELEASE"
                   :exclusions [[org.slf4j/slf4j-api]
                                [org.slf4j/slf4j-log4j12]]]
-                 [slingshot "0.12.2"]]
+                 [org.slf4j/slf4j-api "1.7.12"]
+                 [org.slf4j/slf4j-log4j12 "1.7.12"]
+                 [slingshot "0.12.2"]
+                 [org.iplantc/clojure-commons "5.0.0"]]
   :repositories [["dice.repository"
                   {:url "https://raw.github.com/DICE-UNC/DICE-Maven/master/releases"}]
                  ["iplant.repository"
