5eb7e286519af4260024c4df24eacb28ffcb1d83
Jonathan Strootman <jstroot@iplantcollaborative.org>
2015-08-11 14:59:20 -0700

    Remove dependency to log4j, add logback
    CORE-6977

--- libs/common-cfg/project.clj
+++ libs/common-cfg/project.clj
@@ -4,10 +4,6 @@
   :license {:name "BSD"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.apache.logging.log4j/log4j-api "2.2"]
-                 [org.apache.logging.log4j/log4j-core "2.2"]
-                 [org.apache.logging.log4j/log4j-slf4j-impl "2.2"]
-                 [org.slf4j/slf4j-api "1.7.12"]
                  [com.fasterxml.jackson.core/jackson-core "2.5.1"]
                  [com.fasterxml.jackson.core/jackson-databind "2.5.1"]
                  [com.fasterxml.jackson.core/jackson-annotations "2.5.1"]
--- libs/service-logging/src/service_logging/thread_context.clj
+++ libs/service-logging/src/service_logging/thread_context.clj
@@ -1,19 +1,20 @@
 (ns service-logging.thread-context
-  (:import [org.apache.logging.log4j ThreadContext]))
+  (:import [org.slf4j LoggerFactory MDC]
+           ))
 
 (defn set-context!
   "set-context! puts the data from the context-map into the current thread's
   ThreadContext."
   [context-map]
   (doseq [[k v] (seq context-map)]
-    (ThreadContext/put (name k) v)))
+    (MDC/put (name k) v)))
 
 (defn clear-context!
   "clear-context! clears out context map from the ThreadContext. This should be
   used when the thread is about to be either shut down or returned to a thread
   pool."
   []
-  (ThreadContext/clearMap))
+  (MDC/clear))
 
 (defn wrap-thread-context
   "wrap-thread-context is a ring handler that puts the data in the context-map
