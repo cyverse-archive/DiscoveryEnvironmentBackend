19ce228ba484f7df67855466f9a768b364a9f0d1
Paul Sarando <psarando@iplantcollaborative.org>
2015-08-13 12:05:52 -0700

    CORE-6977 Fixed logging dep in clj-jargon project.
    clj-jargon will not compile in tools that do not depend on service-logging.

--- libs/clj-jargon/project.clj
+++ libs/clj-jargon/project.clj
@@ -5,6 +5,7 @@
             :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.3.1"]
+                 [ch.qos.logback/logback-classic "1.1.3"]
                  [org.irods.jargon/jargon-core "4.0.2.3-RELEASE"
                   :exclusions [[org.jglobus/JGlobus-Core]
                                [org.slf4j/slf4j-api]
