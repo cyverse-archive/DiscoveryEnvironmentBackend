741d591484c31315090d66c8ea7c0ebd2f0ba4fb
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-08-18 09:15:42 -0700

    Removed -a and -t settings from the 'docker run' command in services/docker-build.sh

--- services/docker-build.sh
+++ services/docker-build.sh
@@ -4,6 +4,6 @@ set -e
 
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
 
-docker run --rm -t -a stdout -a stderr -e "GIT_COMMIT=$(git rev-parse HEAD)" -v $(pwd):/build -v ~/.m2:/root/.m2 -w /build discoenv/buildenv lein uberjar
+docker run --rm -e "GIT_COMMIT=$(git rev-parse HEAD)" -v $(pwd):/build -v ~/.m2:/root/.m2 -w /build discoenv/buildenv lein uberjar
 docker build --rm -t "$DOCKER_USER/$DOCKER_REPO:dev" .
 docker push $DOCKER_USER/$DOCKER_REPO:dev
