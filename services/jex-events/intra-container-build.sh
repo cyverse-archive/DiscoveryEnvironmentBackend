3996e1e9763151adc0855cae73a7eb183cb98d67
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-08-20 13:42:42 -0700

    Minor fixes to the build for jex-events for Go 1.5.

--- services/jex-events/intra-container-build.sh
+++ services/jex-events/intra-container-build.sh
@@ -5,4 +5,4 @@ set -e
 
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
 godep restore
-go build --ldflags "-X main.appver $VERSION -X main.gitref $GIT_COMMIT -X main.builtby $BUILD_USER" .
+go build --ldflags "-X main.appver=$VERSION -X main.gitref=$GIT_COMMIT -X main.builtby=$BUILD_USER" .
