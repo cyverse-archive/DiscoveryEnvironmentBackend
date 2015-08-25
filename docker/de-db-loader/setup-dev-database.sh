9860ccf9f42e9d9907e11257b1975c0d7a0427ae
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-08-25 10:48:21 -0700

    More fixes for de-db-loader.

--- docker/de-db-loader/setup-dev-database.sh
+++ docker/de-db-loader/setup-dev-database.sh
@@ -1,19 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 set -x
 set -e
+
+
+env
 
 PGHOST="$POSTGRES_PORT_5432_TCP_ADDR"
 PGPORT="$POSTGRES_PORT_5432_TCP_PORT"
 PGADMIN="postgres"
 PGADMINPASS="$POSTGRES_ENV_POSTGRES_PASSWORD"
 
-cat << EOF >> /root/.pgpass
+cat << EOF >> ~/.pgpass
 $PGHOST:$PGPORT:*:postgres:$PGADMINPASS
 $PGHOST:$PGPORT:*:de:notprod
 EOF
 
-chmod 0600 /root/.pgpass
+chmod 0600 ~/.pgpass
 
 psql -h $PGHOST -p $PGPORT -U $PGADMIN -c "CREATE ROLE de WITH PASSWORD 'notprod' LOGIN;"
 psql -h $PGHOST -p $PGPORT -U $PGADMIN -c "CREATE DATABASE de WITH OWNER de;"
