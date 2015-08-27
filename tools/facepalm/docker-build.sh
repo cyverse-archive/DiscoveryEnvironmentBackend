62eb9b9640ea2b0e9afb2fdc31bfe3092678bd3d
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-08-27 14:16:39 -0700

    Fixed the docker-build.sh file for facepalm.

--- tools/facepalm/docker-build.sh
+++ tools/facepalm/docker-build.sh
@@ -2,6 +2,6 @@
 set -x
 set -e
 
-docker run --rm -t -a stdout -a stderr -e "GIT_COMMIT=$(git rev-parse HEAD)" -v $(pwd):/build -v ~/.m2:/root/.m2 -w /build discoenv/buildenv lein uberjar
+docker run --rm -t -a stdout -a stderr -e "GIT_COMMIT=$(git rev-parse HEAD)" -v $(pwd):/build -v ~/.m2:/root/.m2 -w /build $DOCKER_USER/buildenv lein uberjar
 docker build --rm -t $DOCKER_USER/facepalm:dev .
 docker push $DOCKER_USER/facepalm:dev
