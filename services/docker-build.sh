631f54991bc3b52aec780abd4165b57614705a0a
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-08-27 15:14:09 -0700

    Fixed a missed discoenv.

--- services/docker-build.sh
+++ services/docker-build.sh
@@ -5,7 +5,7 @@ set -e
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
 
 docker pull $DOCKER_USER/buildenv:latest
-docker run --rm -e "GIT_COMMIT=$(git rev-parse HEAD)" -v $(pwd):/build -w /build discoenv/buildenv lein uberjar
+docker run --rm -e "GIT_COMMIT=$(git rev-parse HEAD)" -v $(pwd):/build -w /build $DOCKER_USER/buildenv lein uberjar
 docker build --rm -t "$DOCKER_USER/$DOCKER_REPO:dev" .
 docker push $DOCKER_USER/$DOCKER_REPO:dev
 docker rmi $DOCKER_USER/$DOCKER_REPO:dev
