7b9546867c648d757bc4aa182d8d79474111ba21
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-08-27 15:19:18 -0700

    More parameterization of docker-build.sh

--- services/jex-events/docker-build.sh
+++ services/jex-events/docker-build.sh
@@ -6,7 +6,7 @@ DOCKER_USER=discoenv
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
 
 docker pull $DOCKER_USER/clm-builder:latest
-docker run --rm -e "GIT_COMMIT=$(git rev-parse HEAD)" -e "BUILD_USER=$(whoami)" -v $(pwd):/jex-events -v $(pwd)/intra-container-build.sh:/bin/intra-container-build.sh -w /jex-events discoenv/clm-builder
+docker run --rm -e "GIT_COMMIT=$(git rev-parse HEAD)" -e "BUILD_USER=$(whoami)" -v $(pwd):/jex-events -v $(pwd)/intra-container-build.sh:/bin/intra-container-build.sh -w /jex-events $DOCKER_USER/clm-builder
 docker build --rm -t "$DOCKER_USER/$DOCKER_REPO:dev" .
 docker push $DOCKER_USER/$DOCKER_REPO:dev
 docker rmi $DOCKER_USER/$DOCKER_REPO:dev
