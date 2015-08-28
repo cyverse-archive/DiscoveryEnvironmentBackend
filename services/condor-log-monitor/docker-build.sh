6043988ba397974f46c40a1c2a977e8d33b34429
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-08-28 15:32:10 -0700

    Switched clm to buildenv.

--- services/condor-log-monitor/docker-build.sh
+++ services/condor-log-monitor/docker-build.sh
@@ -5,7 +5,7 @@ set -e
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
 
 docker pull $DOCKER_USER/clm-builder:latest
-docker run --rm -e "GIT_COMMIT=$(git rev-parse HEAD)" -e "BUILD_USER=$(whoami)" -v $(pwd):/condor-log-monitor  -v $(pwd)/intra-container-build.sh:/bin/intra-container-build.sh -w /condor-log-monitor $DOCKER_USER/clm-builder
+docker run --rm -e "GIT_COMMIT=$(git rev-parse HEAD)" -e "BUILD_USER=$(whoami)" -v $(pwd):/condor-log-monitor  -v $(pwd)/intra-container-build.sh:/bin/intra-container-build.sh -w /condor-log-monitor $DOCKER_USER/buildenv:dev
 docker build --rm -t "$DOCKER_USER/$DOCKER_REPO:dev" .
 docker push $DOCKER_USER/$DOCKER_REPO:dev
 docker rmi $DOCKER_USER/$DOCKER_REPO:dev
