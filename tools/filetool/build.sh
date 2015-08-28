4b5b92fa53787b00b95a65de5ae185faba162dec
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-08-28 09:50:57 -0700

    Fixed porklock's build.sh file.

--- tools/filetool/build.sh
+++ tools/filetool/build.sh
@@ -2,28 +2,6 @@
 set -x
 set -e
 
-ITERATION=$1
-USER=iplant
-GROUP=iplant
-BINNAME=porklock
-BUILDDIR=$BINNAME-build
-BINDIR=/usr/local/lib/$BINNAME
-LOGDIR=/var/log/$BINNAME
-PATHBIN=/usr/local/bin
-
-VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
-if [ -d "$BUILDDIR" ]; then
-  rm -r $BUILDDIR
-fi
-mkdir -p $BUILDDIR/$BINDIR
-mkdir -p $BUILDDIR/$LOGDIR
-mkdir -p $BUILDDIR/$PATHBIN
-lein clean
-lein deps
-lein uberjar
-cp curl_wrapper.pl $BUILDDIR/$PATHBIN
-cp porklock $BUILDDIR/$PATHBIN
-cp target/$BINNAME-standalone.jar $BUILDDIR/$BINDIR
-chmod 755 $BUILDDIR/$PATHBIN/curl_wrapper.pl
-chmod 755 $BUILDDIR/$PATHBIN/porklock
-fpm -s dir -t rpm --directories $LOGDIR -d java-1.7.0-openjdk --version $VERSION --iteration $ITERATION --epoch 0 --prefix / --name $BINNAME --verbose -C $BUILDDIR --rpm-user $USER --rpm-group $GROUP -f .
+docker run --rm -e "GIT_COMMIT=$(git rev-parse HEAD)" -v $(pwd):/build -w /build $DOCKER_USER/buildenv lein uberjar
+docker build --rm -t $DOCKER_USER/porklock:dev .
+docker push $DOCKER_USER/porklock:dev
