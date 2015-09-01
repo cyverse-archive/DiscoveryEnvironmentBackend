09e5870443f68d2d4c5f6507312dc3f6ae9e9047
Tony Edgin <tedgin@iplantcollaborative.org>
2015-09-01 12:48:38 -0700

    updated clojure-commons dependencies

--- libs/iplant-clojure-commons/project.clj
+++ libs/iplant-clojure-commons/project.clj
@@ -5,13 +5,14 @@
             :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [cheshire "5.3.1"]
-                 [clj-http "1.0.0"]
-                 [clj-time "0.8.0"]
+                 [cheshire "5.5.0"]
+                 [clj-http "2.0.0"]
+                 [clj-time "0.11.0"]
                  [com.cemerick/url "0.1.1"]
-                 [commons-configuration/commons-configuration "1.10"]  ; provides org.apache.commons.configuration
+                 [commons-configuration "1.10"    ; provides org.apache.commons.configuration
+                  :exclusions [commons-logging]]
                  [me.raynes/fs "1.4.6"]
-                 [medley "0.5.5"]
-                 [slingshot "0.10.3"]
+                 [medley "0.7.0"]
+                 [slingshot "0.12.2"]
                  [trptcolin/versioneer "0.2.0"]]
   :profiles {:test {:resource-paths ["resources" "test-resources"]}})
