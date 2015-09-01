239d89b741a2a2441ecd7e94a0f413d626a4324c
Tony Edgin <tedgin@iplantcollaborative.org>
2015-09-01 15:10:52 -0700

    Noted that service-logging exports tools.logging

--- libs/service-logging/project.clj
+++ libs/service-logging/project.clj
@@ -4,7 +4,7 @@
   :license {:name "BSD"
             :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/tools.logging "0.3.1"]       ; exported
                  [ch.qos.logback/logback-classic "1.1.3"]
                  [net.logstash.logback/logstash-logback-encoder "4.3"]
                  [com.fasterxml.jackson.core/jackson-core "2.5.1"]
