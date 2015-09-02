aa884a4635cebe384d356d8f74b36e0e42eaed53
Ian McEwen <mian@iplantcollaborative.org>
2015-09-02 16:15:18 -0700

    CORE-6217: Add a DELETE /data/:data-id/children endpoint to remove contents of a directory (for /secured/filesystem/delete-contents)

---
+++ services/data-info/src/data_info/routes/trash.clj
@@ -0,0 +1,20 @@
+(ns data-info.routes.trash
+  (:use [compojure.api.sweet]
+        [data-info.routes.domain.common])
+  (:require [data-info.services.trash :as trash]
+            [data-info.util.service :as svc]))
+
+(defroutes* trash
+    (context* "/data/:data-id" []
+      :path-params [data-id :- DataIdPathParam]
+      :tags ["data-by-id"]
+
+      (DELETE* "/children" [:as {uri :uri}]
+        :query [params SecuredQueryParamsRequired]
+        :return Paths
+        :summary "Delete Data Item Contents"
+        :description (str
+  "Deletes the contents of the folder with the provided UUID."
+  (get-error-code-block
+    "ERR_NOT_A_FOLDER, ERR_DOES_NOT_EXIST, ERR_NOT_WRITEABLE, ERR_TOO_MANY_PATHS, ERR_NOT_A_USER"))
+        (svc/trap uri trash/do-delete-uuid-contents params data-id))))
---
+++ services/data-info/src/data_info/services/trash.clj
@@ -0,0 +1,93 @@
+(ns data-info.services.trash
+  (:use [clojure-commons.error-codes]
+        [clj-jargon.init :only [with-jargon]]
+        [clj-jargon.item-ops]
+        [clj-jargon.item-info]
+        [clj-jargon.metadata]
+        [clj-jargon.permissions]
+        [clj-jargon.tickets]
+        [slingshot.slingshot :only [try+ throw+]])
+  (:require [clojure.tools.logging :as log]
+            [clojure.string :as string]
+            [clojure-commons.file-utils :as ft]
+            [dire.core :refer [with-pre-hook! with-post-hook!]]
+            [data-info.util.config :as cfg]
+            [data-info.util.paths :as paths]
+            [data-info.util.logging :as dul]
+            [data-info.services.directory :as directory]
+            [data-info.services.uuids :as uuids]
+            [data-info.util.validators :as validators]))
+
+(def alphanums (concat (range 48 58) (range 65 91) (range 97 123)))
+
+(defn- rand-str
+  [length]
+  (apply str (take length (repeatedly #(char (rand-nth alphanums))))))
+
+(defn- randomized-trash-path
+  [user path-to-inc]
+  (ft/path-join
+   (paths/user-trash-path user)
+   (str (ft/basename path-to-inc) "." (rand-str 7))))
+
+(defn- move-to-trash
+  [cm p user]
+  (let [trash-path (randomized-trash-path user p)]
+    (move cm p trash-path :user user :admin-users (cfg/irods-admins))
+    (set-metadata cm trash-path "ipc-trash-origin" p paths/IPCSYSTEM)))
+
+(defn- delete-paths
+  [user paths]
+  (let [home-matcher #(= (str "/" (cfg/irods-zone) "/home/" user)
+                         (ft/rm-last-slash %1))]
+    (with-jargon (cfg/jargon-cfg) [cm]
+      (let [paths (mapv ft/rm-last-slash paths)]
+        (validators/user-exists cm user)
+        (validators/all-paths-exist cm paths)
+        (validators/user-owns-paths cm user paths)
+
+        ;;; Not allowed to delete the user's home directory.
+        (when (some true? (mapv home-matcher paths))
+          (throw+ {:error_code ERR_NOT_AUTHORIZED
+                   :paths (filterv home-matcher paths)}))
+
+        (doseq [p paths]
+          (log/debug "path" p)
+          (log/debug "readable?" user (owns? cm user p))
+
+          ;;; Delete all of the tickets associated with the file.
+          (let [path-tickets (mapv :ticket-id (ticket-ids-for-path cm (:username cm) p))]
+            (doseq [path-ticket path-tickets]
+              (delete-ticket cm (:username cm) path-ticket)))
+
+          ;;; If the file isn't already in the user's trash, move it there
+          ;;; otherwise, do a hard delete.
+          (if-not (.startsWith p (paths/user-trash-path user))
+            (move-to-trash cm p user)
+            (delete cm p true))) ;;; Force a delete to bypass proxy user's trash.
+
+         {:paths paths}))))
+
+(defn- delete-uuid-contents
+  "Delete contents by UUID: given a user and a data item UUID, delete the contents, returning a list of filenames deleted."
+  [user source-uuid]
+  (let [source (ft/rm-last-slash (:path (uuids/path-for-uuid user source-uuid)))]
+    (with-jargon (cfg/jargon-cfg) [cm]
+      (validators/validate-num-paths-under-folder user source)
+      (validators/path-is-dir cm source))
+    (let [paths (directory/get-paths-in-folder user source)]
+      (delete-paths user paths))))
+
+(defn do-delete-uuid-contents
+  [{user :user} data-id]
+  (delete-uuid-contents user data-id))
+
+(with-pre-hook! #'do-delete-uuid-contents
+  (fn [params data-id]
+    (dul/log-call "do-delete-uuid-contents" params data-id)
+
+    (when (paths/super-user? (:user params))
+      (throw+ {:error_code ERR_NOT_AUTHORIZED
+               :user       (:user params)}))))
+
+(with-post-hook! #'do-delete-uuid-contents (dul/log-func "do-delete-uuid-contents"))
