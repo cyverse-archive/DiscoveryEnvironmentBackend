a9d968a217e882c1beb34f528a984961b9eabe9b
Ian McEwen <mian@iplantcollaborative.org>
2015-09-09 16:27:52 -0700

    Use and provide more effective macros for manipulating the MDC context in service-logging.thread-context.

--- libs/service-logging/src/service_logging/thread_context.clj
+++ libs/service-logging/src/service_logging/thread_context.clj
@@ -4,32 +4,44 @@
 
 (defn set-context!
   "set-context! puts the data from the context-map into the current thread's
-  ThreadContext."
+  context."
   [context-map]
-  (doseq [[k v] (seq context-map)]
-    (MDC/put (name k) v)))
+  (if (map? context-map)
+    (doall (map (fn [[k# v#]] (MDC/put (name k#) (str v#))) context-map))))
 
 (defn clear-context!
-  "clear-context! clears out context map from the ThreadContext. This should be
+  "clear-context! clears out context map from the context. This should be
   used when the thread is about to be either shut down or returned to a thread
   pool."
   []
   (MDC/clear))
+
+;; With thanks to https://github.com/vaughnd/clojure-example-logback-integration/
+(defmacro with-logging-context [context & body]
+  "Use this to add a map to any logging wrapped in the macro. Macro can be nested.
+  (with-logging-context {:key \"value\"} (log/info \"yay\"))
+  "
+  `(let [wrapped-context# ~context
+         ctx# (MDC/getCopyOfContextMap)]
+     (try
+       (set-context! wrapped-context#)
+       ~@body
+       (finally
+         (if ctx#
+           (MDC/setContextMap ctx#)
+           (clear-context!))))))
 
 (defn wrap-thread-context
   "wrap-thread-context is a ring handler that puts the data in the context-map
   into the request thread's ThreadContext."
   [handler context-map]
   (fn [request]
-    (set-context! context-map)
-    (let [resp (handler request)]
-      (clear-context!)
-      resp)))
+    (with-logging-context context-map
+      (handler request))))
 
 (defn add-user-to-context
   "add-user-to-context is a ring handler that adds the user value from the query
-  string into the ThreadContext with a key of 'username'. The query params
-  need to be parsed first."
+  string into the context with a key of 'user'. The query params need to be parsed first."
   [handler]
   (fn [request]
     (let [q-params (:query-params request)]
