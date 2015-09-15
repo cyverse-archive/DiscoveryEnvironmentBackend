fba12cbec94897d4cb135acc5d5c1cde7163f0b4
Ian McEwen <mian@iplantcollaborative.org>
2015-09-15 11:54:59 -0700

    Remove uuid-corrector project.

--- tools/uuid-corrector/docker-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -x
-set -e
-
-docker pull discoenv/buildenv:latest
-docker run --rm -t -a stdout -a stderr -e "GIT_COMMIT=$(git rev-parse HEAD)" -v $(pwd):/build -w /build discoenv/buildenv lein uberjar
-docker build --rm -t $DOCKER_USER/uuid-corrector:dev .
-docker push $DOCKER_USER/uuid-corrector:dev
