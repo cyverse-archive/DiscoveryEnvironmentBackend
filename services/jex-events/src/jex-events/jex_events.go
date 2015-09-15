8d70191b6003041649d131d90dff5e5852efcccd
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-09-15 07:33:46 -0700

    Switched the mode to a flag and fixed logging in monitor mode.

--- services/jex-events/src/jex-events/jex_events.go
+++ services/jex-events/src/jex-events/jex_events.go
@@ -12,19 +12,18 @@ import (
 )
 
 var (
-	cfgPath = flag.String("config", "", "Path to the config value")
+	cfgPath = flag.String("config", "", "Path to the config value. Required.")
+	mode    = flag.String("mode", "", "One of 'monitor' or 'events'. Required.")
 	version = flag.Bool("version", false, "Print the version information")
 	gitref  string
 	appver  string
 	builtby string
 	logger  *log.Logger
-	args    []string
 )
 
 func init() {
 	logger = log.New(logcabin.LoggerFunc(logcabin.LogWriter), "", log.Lshortfile)
 	flag.Parse()
-	args = flag.Args()
 }
 
 // AppVersion prints version information to stdout
@@ -41,87 +40,53 @@ func AppVersion() {
 	}
 }
 
-func help() string {
-	h := `
-jex-events, services for monitoring DE jobs.
-
-Usage:
-
-	jex-events mode [arguments]
-
-The modes are:
-
-	monitor     Monitor Condor's event_log for changes
-	events      Listen for events parsed by monitor
-	help        Prints out this help message
-
-Each mode accepts the following options:
-
-	--config    The path to the configuration file. Required.
-	--version   Print out version information`
-	return h
-}
-
 func main() {
 	if *version {
 		AppVersion()
 		os.Exit(0)
 	}
 
-	if len(args) == 0 {
-		fmt.Println("No mode specified")
-		fmt.Println(help())
-		os.Exit(-1)
-	}
-
-	if len(args) > 1 {
-		fmt.Println("Multiple modes specified")
-		fmt.Println(help())
-		os.Exit(-1)
-	}
-
-	validModes := []string{"monitor", "events", "help"}
-	mode := args[0]
+	validModes := []string{"monitor", "events"}
 	foundMode := false
 
 	for _, v := range validModes {
-		if v == mode {
+		if v == *mode {
 			foundMode = true
 		}
 	}
 
 	if !foundMode {
-		fmt.Printf("Invalid mode: %s\n", mode)
-		fmt.Println(help())
+		fmt.Printf("Invalid mode: %s\n", *mode)
+		flag.PrintDefaults()
 		os.Exit(-1)
 	}
 
 	if *cfgPath == "" {
 		fmt.Println("Error: --config must be set.")
-		fmt.Println(help())
+		flag.PrintDefaults()
 		os.Exit(-1)
 	}
+
 	cfg, err := configurate.New(*cfgPath, logger)
 	if err != nil {
 		logger.Print(err)
 		os.Exit(-1)
 	}
+
 	logger.Println("Done reading config.")
 	if !cfg.Valid() {
 		logger.Println("Something is wrong with the config file.")
 		os.Exit(-1)
 	}
 
-	switch mode {
-	case "help":
-		fmt.Println(help())
+	switch *mode {
 	case "events":
 		events.Run(cfg, logger)
 	case "monitor":
 		monitor.Run(cfg, logger)
 	default:
 		fmt.Println("Bad mode! Bad! Look what you did!")
-		fmt.Println(help())
+		flag.PrintDefaults()
 		os.Exit(-1)
 	}
 }
