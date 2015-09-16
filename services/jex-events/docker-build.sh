b601b55e6b54051ef2bf6c91e22d2fbb8925bcc6
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-09-16 14:05:01 -0700

    jex-events is built with buildenv now.

--- services/jex-events/docker-build.sh
+++ services/jex-events/docker-build.sh
@@ -4,8 +4,8 @@ set -e
 
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
 
-docker pull $DOCKER_USER/clm-builder:latest
-docker run --rm -e "GIT_COMMIT=$(git rev-parse HEAD)" -e "BUILD_USER=$(whoami)" -v $(pwd):/jex-events -v $(pwd)/intra-container-build.sh:/bin/intra-container-build.sh -w /jex-events $DOCKER_USER/clm-builder
+docker pull $DOCKER_USER/buildenv:latest
+docker run --rm -e "GIT_COMMIT=$(git rev-parse HEAD)" -e "BUILD_USER=$(whoami)" -v $(pwd):/jex-events -v $(pwd)/intra-container-build.sh:/bin/intra-container-build.sh -w /jex-events $DOCKER_USER/buildenv:latest
 docker build --rm -t "$DOCKER_USER/$DOCKER_REPO:dev" .
 docker push $DOCKER_USER/$DOCKER_REPO:dev
 docker rmi $DOCKER_USER/$DOCKER_REPO:dev
