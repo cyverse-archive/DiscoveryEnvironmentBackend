81a3bea14e92c72c9a6091ee1fd7e2c773462504
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-09-17 15:51:19 -0700

    More unit tests.

--- services/jex-events/src/submissions/transforms_test.go
+++ services/jex-events/src/submissions/transforms_test.go
@@ -44,13 +44,97 @@ func TestJSONParsing(t *testing.T) {
 func TestDescription(t *testing.T) {
 	s := inittests(t)
 	if s.Description != "this is a description" {
-		t.Error("The description is wrong")
+		t.Errorf("The description was '%s' instead of 'this is a description'", s.Description)
 	}
 }
 
 func TestEmail(t *testing.T) {
 	s := inittests(t)
 	if s.Email != "wregglej@iplantcollaborative.org" {
-		t.Error("The email is wrong")
+		t.Errorf("The email was '%s' instead of wregglej@iplantcollaborative.org", s.Email)
+	}
+}
+
+func TestName(t *testing.T) {
+	s := inittests(t)
+	if s.Name != "Word_Count_analysis1" {
+		t.Errorf("The name field was '%s' instead of 'Word_Count_analysis1'", s.Name)
+	}
+}
+
+func TestUsername(t *testing.T) {
+	s := inittests(t)
+	if s.Username != "wregglej" {
+		t.Errorf("The username was '%s' instead of 'wregglej'", s.Username)
+	}
+}
+
+func TestAppID(t *testing.T) {
+	s := inittests(t)
+	if s.AppID != "c7f05682-23c8-4182-b9a2-e09650a5f49b" {
+		t.Errorf("The app_id was '%s' instead of 'c7f05682-23c8-4182-b9a2-e09650a5f49b'", s.AppID)
+	}
+}
+
+func TestCreateOutputSubdir(t *testing.T) {
+	s := inittests(t)
+	if !s.CreateOutputSubdir {
+		t.Errorf("create_output_subdir was false when it should have been true")
+	}
+}
+
+func TestRequestType(t *testing.T) {
+	s := inittests(t)
+	if s.RequestType != "submit" {
+		t.Errorf("request_type was '%s' instead of 'submit'", s.RequestType)
+	}
+}
+
+func TestAppDescription(t *testing.T) {
+	s := inittests(t)
+	if s.AppDescription != "this is an app description" {
+		t.Errorf("app_description was '%s' instead of 'this is an app description'", s.AppDescription)
+	}
+}
+
+func TestOutputDir(t *testing.T) {
+	s := inittests(t)
+	if s.OutputDir != "/iplant/home/wregglej/analyses/Word_Count_analysis1-2015-09-17-21-42-20.9" {
+		t.Errorf("output_dir was '%s' instead of '/iplant/home/wregglej/analyses/Word_Count_analysis1-2015-09-17-21-42-20.9'", s.OutputDir)
+	}
+}
+
+func TestWikiURL(t *testing.T) {
+	s := inittests(t)
+	if s.WikiURL != "https://pods.iplantcollaborative.org/wiki/display/DEapps/WordCount" {
+		t.Errorf("wiki_url was '%s' instead of 'https://pods.iplantcollaborative.org/wiki/display/DEapps/WordCount'", s.WikiURL)
+	}
+}
+
+func TestUUID(t *testing.T) {
+	s := inittests(t)
+	if s.UUID != "07b04ce2-7757-4b21-9e15-0b4c2f44be26" {
+		t.Errorf("uuid was '%s' instead of '07b04ce2-7757-4b21-9e15-0b4c2f44be26'", s.UUID)
+	}
+}
+
+func TestNotify(t *testing.T) {
+	s := inittests(t)
+	if !s.Notify {
+		t.Errorf("notify was false instead of true")
+	}
+}
+
+func TestExecutionTarget(t *testing.T) {
+	s := inittests(t)
+	if s.ExecutionTarget != "condor" {
+		t.Errorf("execution_target was '%s' instead of 'condor'", s.ExecutionTarget)
+	}
+}
+
+func TestAppName(t *testing.T) {
+	s := inittests(t)
+	if s.AppName != "Word Count" {
+		t.Errorf("app_name was '%s' instead of 'Word Count'", s.AppName)
 	}
 }
