4b7f12d909b42e2c3c0344a05fec019b6832ef2b
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-09-17 10:44:19 -0700

    Cleaned up jex-events' docker-build.sh file.

--- services/jex-events/docker-build.sh
+++ services/jex-events/docker-build.sh
@@ -2,10 +2,25 @@
 set -x
 set -e
 
+if [ -z "$DOCKER_USER" ]; then
+	DOCKER_USER=discoenv
+fi
+
+if [ -z "$DOCKER_REPO" ]; then
+	DOCKER_REPO=jex-events
+fi
+
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
 
 docker pull $DOCKER_USER/buildenv:latest
-docker run --rm -e "GIT_COMMIT=$(git rev-parse HEAD)" -e "BUILD_USER=$(whoami)" -v $(pwd):/jex-events -v $(pwd)/intra-container-build.sh:/bin/intra-container-build.sh -w /jex-events $DOCKER_USER/buildenv:latest
+docker run --rm  \
+	-e "VERSION=$VERISON" \
+	-e "GIT_COMMIT=$(git rev-parse HEAD)" \
+	-e "BUILD_USER=$(whoami)" \
+	-v $(pwd):/jex-events \
+	-w /jex-events \
+	$DOCKER_USER/buildenv:latest \
+	gb build --ldflags "-X main.appver=$VERSION -X main.gitref=$GIT_COMMIT -X main.builtby=$BUILD_USER"
 docker build --rm -t "$DOCKER_USER/$DOCKER_REPO:dev" .
 docker push $DOCKER_USER/$DOCKER_REPO:dev
 docker rmi $DOCKER_USER/$DOCKER_REPO:dev
--- services/jex-events/intra-container-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -x
-set -e
-
-VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
-gb build --ldflags "-X main.appver=$VERSION -X main.gitref=$GIT_COMMIT -X main.builtby=$BUILD_USER"
