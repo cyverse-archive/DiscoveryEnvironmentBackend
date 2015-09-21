3b56d62a7625065e683b74d0dba851a9b6165376
Ian McEwen <mian@iplantcollaborative.org>
2015-09-21 09:57:28 -0700

    CORE-6256: Add PUT /data/:data-id/type to data-info for setting/unsetting the filetype for a file.

--- services/data-info/src/data_info/routes/filetypes.clj
+++ services/data-info/src/data_info/routes/filetypes.clj
@@ -1,5 +1,6 @@
 (ns data-info.routes.filetypes
-  (:use [compojure.api.sweet]
+  (:use [common-swagger-api.schema]
+        [data-info.routes.domain.common]
         [data-info.routes.domain.filetypes])
   (:require [data-info.services.filetypes :as filetypes]
             [data-info.util.service :as svc]))
@@ -10,4 +11,15 @@
     :return TypesList
     :summary "List File Types"
     :description "Lists available file types supported by the underlying library heuristomancer."
-    (svc/trap uri filetypes/do-type-list)))
+    (svc/trap uri filetypes/do-type-list))
+
+  (PUT* "/data/:data-id/type" [:as {uri :uri}]
+    :tags ["data-by-id"]
+    :query [params StandardUserQueryParams]
+    :path-params [data-id :- DataIdPathParam]
+    :body [body (describe FileType "The type to set.")]
+    :return FileTypeReturn
+    :summary "Set File Type"
+    :description (str "Set a file's type to a specific value, or unset it with an empty string."
+(get-error-code-block "ERR_NOT_OWNER, ERR_BAD_OR_MISSING_FIELD,ERR_DOES_NOT_EXIST,ERR_NOT_A_USER"))
+    (svc/trap uri filetypes/do-add-type-uuid params body data-id)))
