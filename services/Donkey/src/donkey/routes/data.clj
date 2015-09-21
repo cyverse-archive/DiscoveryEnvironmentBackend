d6f6b9aa85559aa2407b0935c453976c20a99cb2
Ian McEwen <mian@iplantcollaborative.org>
2015-09-21 10:30:52 -0700

    CORE-6256: Use data-info PUT /data/:data-id/type for Donkey POST /secured/filesystem/type

--- services/Donkey/src/donkey/routes/data.clj
+++ services/Donkey/src/donkey/routes/data.clj
@@ -5,7 +5,6 @@
         [donkey.auth.user-attributes]
         [donkey.util])
   (:require [donkey.util.config :as config]
-            [donkey.services.filesystem.garnish.controllers :as garnish]
             [donkey.clients.data-info :as data]
             [donkey.clients.saved-searches :as saved]))
 
@@ -15,11 +14,11 @@
   (optional-routes
    [config/data-routes-enabled]
 
-   (POST "/filetypes/type" [:as {:keys [body params]}]
-         (garnish/add-type body params))
+   (POST "/filetypes/type" [:as req]
+      (controller req data/set-file-type :params :body))
 
-   (GET "/filetypes/type-list" []
-        (data/get-type-list))
+   (GET "/filetypes/type-list" [:as req]
+      (controller req data/get-type-list))
 
    (POST "/share" [:as req]
          (share req))
--- services/Donkey/src/donkey/services/filesystem/garnish/irods.clj
+++ services/Donkey/src/donkey/services/filesystem/garnish/irods.clj
@@ -12,54 +12,6 @@
             [donkey.services.filesystem.icat :as icat])
   (:import [org.apache.tika Tika]))
 
-(defn add-type
-  "Adds the type to a file in iRODS at path for the specified user."
-  ([user path type]
-    (with-jargon (icat/jargon-cfg) [cm]
-      (add-type cm user path type)))
-
-  ([cm user path type]
-    (log/info "in add-type")
-
-    (when-not (exists? cm path)
-      (throw+ {:error_code ERR_DOES_NOT_EXIST
-               :path path}))
-
-    (when-not (user-exists? cm user)
-      (throw+ {:error_code ERR_NOT_A_USER
-               :user user}))
-
-    (when-not (is-writeable? cm user path)
-      (throw+ {:error_code ERR_NOT_OWNER
-               :user user
-               :path path}))
-    (set-metadata cm path (cfg/garnish-type-attribute) type "")
-    (log/info "Added type " type " to " path " for " user ".")
-    {:path path
-     :type type}))
-
-(defn unset-types
-  "Removes all info-type associations from a path."
-  [user path]
-  (log/info "in unset-type")
-
-  (with-jargon (icat/jargon-cfg) [cm]
-    (when-not (exists? cm path)
-      (throw+ {:error_code ERR_DOES_NOT_EXIST
-               :path path}))
-
-    (when-not (user-exists? cm user)
-      (throw+ {:error_code ERR_NOT_A_USER
-               :user user}))
-
-    (when-not (is-writeable? cm user path)
-      (throw+ {:error_code ERR_NOT_OWNER
-               :user user
-               :path path}))
-    (delete-metadata cm path (cfg/garnish-type-attribute))
-    (log/info "Deleted types from" path "for" user)
-    {:path path :user user}))
-
 (defn get-types
   "Gets all of the filetypes associated with path."
   ([cm user path]
