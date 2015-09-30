5bfffdd24bd687ac1d141eb5fac6b88b7e5fe045
Dennis Roberts <dennis@iplantcollaborative.org>
2015-09-30 11:29:58 -0700

    CORE-7088: made some changes based on code review suggestions

--- tools/make-jwt/src/configs/configs.go
+++ tools/make-jwt/src/configs/configs.go
@@ -23,6 +23,9 @@ type ParamFile struct {
 	scanner *bufio.Scanner
 }
 
+// NewParamFile attempts to create a new ParamFile structure for the file at the given path. If
+// the file exists, is a regular file and can be opened then the new ParamFile structure is
+// returned. Otherwise, an error is returned.
 func NewParamFile(path string) (*ParamFile, error) {
 
 	// Verify that the file is a regular file.
@@ -43,6 +46,9 @@ func NewParamFile(path string) (*ParamFile, error) {
 	return &ParamFile{path, file, bufio.NewScanner(file)}, nil
 }
 
+// NextParam advances the ParamFile structure to the next parameter definition in the file. Blank
+// lines and comments (lines beginning with a hash mark) are skipped. Returns `true` if another
+// parameter definition was found in the file, `false` otherwise.
 func (p *ParamFile) NextParam() bool {
 
 	// Search for the next significant line.
@@ -56,6 +62,9 @@ func (p *ParamFile) NextParam() bool {
 	return false
 }
 
+// ExtractParam extracts the current parameter from the file. The behavior of this function is
+// undefined for ParamFile strucures for which NextParam has not been called or for which
+// NextParam returned `false` the last time it was called.
 func (p *ParamFile) ExtractParam() (*string, *string) {
 	line := strings.TrimSpace(p.scanner.Text())
 	strs := regexp.MustCompile("\\s*=\\s*").Split(line, 2)
@@ -66,6 +75,7 @@ func (p *ParamFile) ExtractParam() (*string, *string) {
 	return &strs[0], nil
 }
 
+// Close closes the parameter file.
 func (p *ParamFile) Close() error {
 	return p.file.Close()
 }
