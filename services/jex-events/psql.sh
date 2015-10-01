ae870f6141d3ad485a51e9833762703c4faf451b
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-10-01 12:23:22 -0700

    Added a psql.sh script for the jex.

---
+++ services/jex-events/psql.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "This script and docker container are intended for use against the database created by the ./test.sh script."
+echo ''
+echo "The password for the 'de' user to the 'de' database is 'notprod' (without the quotes)."
+
+docker run -it --link jexdb:postgres --rm postgres sh -c 'exec psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U de -d jex'
