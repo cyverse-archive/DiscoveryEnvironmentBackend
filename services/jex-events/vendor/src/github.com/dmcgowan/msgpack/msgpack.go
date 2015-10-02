5350e71f6054efb3563b94bf647cc227e8e63cf1
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-10-02 13:43:29 -0700

    Removed libchan and deps.

--- services/jex-events/vendor/src/github.com/dmcgowan/msgpack/msgpack.go
+++
@@ -1,9 +0,0 @@
-package msgpack // import "github.com/dmcgowan/msgpack"
-
-type Marshaler interface {
-	MarshalMsgpack() ([]byte, error)
-}
-
-type Unmarshaler interface {
-	UnmarshalMsgpack([]byte) error
-}
