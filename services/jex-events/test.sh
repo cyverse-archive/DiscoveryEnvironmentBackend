dda890c2bf1022f93d91f239340bc5e61ba6feda
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-10-07 10:10:41 -0700

    Moved running unit tests to docker-compose.

--- services/jex-events/test.sh
+++ services/jex-events/test.sh
@@ -3,16 +3,4 @@
 set -e
 set -x
 
-if [ $(docker ps | grep jexdb | wc -l) -eq 0 ]; then
-    echo "Can't find a container named 'jexdb'. Going to try and create it..."
-
-    if [ $(docker images | grep dev | grep unittest-jexdb | wc -l) -eq 0 ]; then
-        echo "Please run create-image.sh in docker/jex-db-loader/"
-        exit 1
-    fi
-
-    docker run --name jexdb -d discoenv/unittest-jexdb:dev
-    sleep 5
-fi
-
-docker run --rm --link jexdb:jexdb -v $(pwd):/jex -w /jex discoenv/buildenv ./dbtest.sh
+docker-compose -f test.yml up --force-recreate
