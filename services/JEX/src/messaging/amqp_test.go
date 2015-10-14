fed447be979146b74cd8f3c2781e37b5d5ee5f63
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-10-14 13:13:15 -0700

    Added test for sending and receiving messages.

--- services/JEX/src/messaging/amqp_test.go
+++ services/JEX/src/messaging/amqp_test.go
@@ -1,6 +1,28 @@
 package messaging
 
-import "testing"
+import (
+	"fmt"
+	"model"
+	"os"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+func shouldrun() bool {
+	if os.Getenv("RABBIT_PORT_5672_TCP_ADDR") != "" {
+		return true
+	}
+	return false
+}
+
+func uri() string {
+	addr := os.Getenv("RABBIT_PORT_5672_TCP_ADDR")
+	port := os.Getenv("RABBIT_PORT_5672_TCP_PORT")
+	return fmt.Sprintf("amqp://guest:guest@%s:%s/", addr, port)
+}
 
 func TestConstants(t *testing.T) {
 	expected := 0
@@ -18,4 +40,67 @@ func TestConstants(t *testing.T) {
 	if actual != expected {
 		t.Errorf("Success was %d instead of %d", actual, expected)
 	}
+}
+
+func TestNewStopRequest(t *testing.T) {
+	actual := NewStopRequest()
+	expected := &StopRequest{Version: 0}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("NewStopRequest returned:\n%#v\n\tinstead of:\n%#v", actual, expected)
+	}
+}
+
+func TestNewLaunchRequest(t *testing.T) {
+	job := &model.Job{}
+	actual := NewLaunchRequest(job)
+	expected := &JobRequest{
+		Version: 0,
+		Job:     job,
+		Command: Launch,
+	}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("NewLaunchRequest returned:\n%#v\n\tinstead of:\n%#v", actual, expected)
+	}
+}
+
+func TestNewClient(t *testing.T) {
+	if !shouldrun() {
+		return
+	}
+	actual := NewClient(uri())
+	defer actual.Close()
+	expected := uri()
+	if actual.uri != expected {
+		t.Errorf("Client's uri was %s instead of %s", actual.uri, expected)
+	}
+}
+
+func TestClient(t *testing.T) {
+	if !shouldrun() {
+		return
+	}
+	client := NewClient(uri())
+	defer client.Close()
+	exchange := "jex_tests"
+	key := "tests"
+	client.SetupPublishing("jex_tests")
+
+	actual := ""
+	expected := "this is a test"
+	coord := make(chan int)
+
+	handler := func(d amqp.Delivery) {
+		d.Ack(false)
+		actual = string(d.Body)
+		coord <- 1
+	}
+	client.AddConsumer(exchange, "test_queue", key, handler)
+	go client.Listen()
+	time.Sleep(100 * time.Millisecond)
+	client.Publish(key, []byte(expected))
+	<-coord
+	if actual != expected {
+		t.Errorf("Handler received %s instead of %s", actual, expected)
+	}
+
 }
