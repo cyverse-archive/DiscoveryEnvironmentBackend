6f06cdc61deedf2bf4793a0dd1ec9bb9ef7bdeac
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-10-14 10:55:28 -0700

    CORE-7134: Added a unit test to make sure the amqp.uri setting is accessible.

--- services/jex-events/src/configurate/configurate_test.go
+++ services/jex-events/src/configurate/configurate_test.go
@@ -18,6 +18,23 @@ func TestNew(t *testing.T) {
 	}
 }
 
+func TestAMQPConfig(t *testing.T) {
+	err := configurator()
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+	actual, err := C.String("amqp.uri")
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+	expected := "amqp://guest:guest@192.168.99.100:5672/"
+	if actual != expected {
+		t.Errorf("The amqp.uri was %s instead of %s", actual, expected)
+	}
+}
+
 // func TestValid(t *testing.T) {
 // 	cfg, err := configurator()
 // 	if err != nil {
