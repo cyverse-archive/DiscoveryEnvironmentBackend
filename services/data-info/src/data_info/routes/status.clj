1fb09d8245f5dd839494cf9e718bfe26db0ad664
Ian McEwen <mian@iplantcollaborative.org>
2015-10-19 10:22:27 -0700

    CORE-6209: add iRODS status to data-info's existing status information.

--- services/data-info/src/data_info/routes/status.clj
+++ services/data-info/src/data_info/routes/status.clj
@@ -2,6 +2,7 @@
   (:use [common-swagger-api.schema]
         [data-info.routes.domain.status])
   (:require [clojure-commons.service :as commons-svc]
+            [data-info.services.status :as status]
             [data-info.util.config :as config]
             [data-info.util.service :as service]
             [schema.core :as s]))
@@ -13,7 +14,8 @@
       :summary "Service Information"
       :description "This endpoint provides the name of the service and its version."
       (service/trap uri
-        commons-svc/get-docs-status config/svc-info server-name server-port config/docs-uri))
+        #(assoc (commons-svc/get-docs-status config/svc-info server-name server-port config/docs-uri)
+                :iRODS (status/irods-running?))))
 
     (GET* "/admin/config" [:as {:keys [uri]}]
       :return (describe s/Any "A map of configuration keys to values.")
