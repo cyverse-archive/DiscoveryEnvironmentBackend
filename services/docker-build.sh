a32a6df1a472388350a878b1159f2fcf4a2995a1
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2015-10-27 21:29:43 -0700

    Add --pull to services' docker build command.
    This should make the images use more shared layers.

--- services/docker-build.sh
+++ services/docker-build.sh
@@ -6,6 +6,6 @@ VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
 
 docker pull $DOCKER_USER/buildenv:latest
 docker run --rm -e "GIT_COMMIT=$(git rev-parse HEAD)" -v $(pwd):/build -w /build $DOCKER_USER/buildenv lein uberjar
-docker build --rm -t "$DOCKER_USER/$DOCKER_REPO:dev" .
+docker build --pull --rm -t "$DOCKER_USER/$DOCKER_REPO:dev" .
 docker push $DOCKER_USER/$DOCKER_REPO:dev
 docker rmi $DOCKER_USER/$DOCKER_REPO:dev
